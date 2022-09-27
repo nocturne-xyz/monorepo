eb79985d1729a7bbc6e33d324a15e82bd7b127a7
Luke Tchang <ltchang@stanford.edu>
2022-09-26 21:40:09 -0700

    forge install: openzeppelin-contracts

---
+++ packages/contracts/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export type { Router } from "./src/Router";
+
+export { Router__factory } from "./src/factories/Router__factory";
+
+export { version } from "./package.json";
