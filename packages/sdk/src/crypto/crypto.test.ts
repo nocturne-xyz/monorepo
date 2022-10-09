695a0e169fe9c50ad34ef3f5973beac74470fda6
Wei Dai <me@wdai.us>
2022-10-09 04:20:50 -0500

    Add address rerandomization and serialization

--- packages/sdk/src/crypto/crypto.test.ts
+++ packages/sdk/src/crypto/crypto.test.ts
@@ -1,4 +1,4 @@
-import { genPriv, privToAddr, testOwn, sign, verify } from "./crypto";
+import { genPriv, privToAddr, testOwn, sign, verify, rerandAddr, addrToString, parseAddr } from "./crypto";
 
 describe("[crypto]", () => {
   test("View key should work", () => {
@@ -9,7 +9,23 @@ describe("[crypto]", () => {
     expect(testOwn(priv2, addr)).toEqual(false);
   });
 
-  test("Sign / verify", () => {
+  test("Test rerand", () => {
+    let priv = genPriv();
+    let addr = privToAddr(priv);
+    let addr2 = rerandAddr(addr);
+    let priv2 = genPriv();
+    expect(testOwn(priv, addr)).toEqual(true);
+    expect(testOwn(priv2, addr)).toEqual(false);
+  });
+
+  test("Test address (de)serielization", () => {
+    let priv = genPriv();
+    let addr = privToAddr(priv);
+    let str = addrToString(addr);
+    expect(parseAddr(str)).toEqual(addr);
+  });
+
+  test("Test Sign / verify", () => {
     let priv = genPriv();
     let addr = privToAddr(priv);
     let m = BigInt(123);
--- packages/sdk/src/crypto/crypto.ts
+++ packages/sdk/src/crypto/crypto.ts
@@ -22,8 +22,8 @@ export function genPriv(): FlaxPrivKey {
     // TODO make vk and sk acutally uniformly distributed
     let vk_buf = randomBytes(Math.floor(256 / 8));
     let sk_buf = randomBytes(Math.floor(256 / 8));
-    let vk = Scalar.fromRprLE(vk_buf, 0, 32) % babyjub.subOrder;
-    let sk = Scalar.fromRprLE(sk_buf, 0, 32) % babyjub.subOrder;
+    let vk = Scalar.fromRprBE(vk_buf, 0, 32) % babyjub.subOrder;
+    let sk = Scalar.fromRprBE(sk_buf, 0, 32) % babyjub.subOrder;
     let priv: FlaxPrivKey = {
         vk: vk,
         sk: sk,
@@ -33,7 +33,7 @@ export function genPriv(): FlaxPrivKey {
 
 export function privToAddr(priv: FlaxPrivKey): FlaxAddr {
     let r_buf = randomBytes(Math.floor(256 / 8));
-    let r = Scalar.fromRprLE(r_buf, 0, 32);
+    let r = Scalar.fromRprBE(r_buf, 0, 32);
     let H1 = babyjub.mulPointEscalar(babyjub.Base8, r);
     let H2 = babyjub.mulPointEscalar(H1, priv.vk);
     let H3 = babyjub.mulPointEscalar(H1, priv.sk);
@@ -45,6 +45,49 @@ export function privToAddr(priv: FlaxPrivKey): FlaxAddr {
     return addr;
 }
 
+export function rerandAddr(addr: FlaxAddr): FlaxAddr {
+    let r_buf = randomBytes(Math.floor(256 / 8));
+    let r = Scalar.fromRprBE(r_buf, 0, 32);
+    let H1 = babyjub.mulPointEscalar(addr.H1, r);
+    let H2 = babyjub.mulPointEscalar(addr.H2, r);
+    let H3 = babyjub.mulPointEscalar(addr.H3, r);
+    return {
+        H1: H1,
+        H2: H2,
+        H3: H3,
+    };
+}
+
+const FlaxAddrPrefix = '0f';
+
+export function addrToString(addr: FlaxAddr): String {
+    let b1 = Buffer.from(babyjub.packPoint(addr.H1));
+    let b2 = Buffer.from(babyjub.packPoint(addr.H2));
+    let b3 = Buffer.from(babyjub.packPoint(addr.H3));
+    console.log(b1.length);
+    console.log(b2.length);
+    console.log(b3.length);
+    let b = Buffer.concat([b1, b2, b3]);
+    return FlaxAddrPrefix + b.toString('base64');
+}
+
+export function parseAddr(str: String): FlaxAddr {
+    let result = str.split(FlaxAddrPrefix);
+    let base64str = str.slice(FlaxAddrPrefix.length);
+    let b = Buffer.from(base64str, 'base64');
+    let b1 = b.slice(0, 32);
+    let b2 = b.slice(32, 64);
+    let b3 = b.slice(64, 96);
+    let H1 = babyjub.unpackPoint(b1);
+    let H2 = babyjub.unpackPoint(b2);
+    let H3 = babyjub.unpackPoint(b3);
+    return {
+        H1: H1,
+        H2: H2,
+        H3: H3,
+    }
+}
+
 export function testOwn(priv: FlaxPrivKey, addr: FlaxAddr): boolean {
     let H2prime = babyjub.mulPointEscalar(addr.H1, priv.vk);
     return (addr.H2[0] === H2prime[0]) && (addr.H2[1] === H2prime[1]);
@@ -53,7 +96,7 @@ export function testOwn(priv: FlaxPrivKey, addr: FlaxAddr): boolean {
 export function sign(priv: FlaxPrivKey, addr: FlaxAddr, m: BigInt) {
     // TODO: make this deterministic
     let r_buf = randomBytes(Math.floor(256 / 8));
-    let r = Scalar.fromRprLE(r_buf, 0, 32);
+    let r = Scalar.fromRprBE(r_buf, 0, 32);
     let R = babyjub.mulPointEscalar(addr.H1, r);
     let c = poseidon([R[0], R[1], m]);
     let z = (r - priv.sk * c) % babyjub.subOrder;
