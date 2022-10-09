95af2a49c7c4998844741ecb82e0e8fc7ac4c27a
Wei Dai <me@wdai.us>
2022-10-08 21:32:58 -0500

    Comment out babyjub-util code and start fresh

--- packages/sdk/src/crypto/babyjub-utils.test.ts
+++ packages/sdk/src/crypto/babyjub-utils.test.ts
@@ -2,41 +2,45 @@ import { privToPub, genPriv } from "./babyjub-utils";
 import { bytesToHex, hexToBytes } from "../utils";
 
 describe("[bayjub utils]", () => {
-  const privKey =
-    "0x28156abe7fe2fd433dc9df969286b96666489bac508612d0e16593e944c4f69f";
-  const privKeyBuff = hexToBytes(privKey);
-
-  test("Generate public key uncompressed", () => {
-    const pubKey = privToPub(privKeyBuff, false);
-    expect(bytesToHex(pubKey)).toEqual(
-      "0x270000b73fba5f79c0491a32d4e64f69813db369ea106c09bc5ca4ae220cbb81" +
-        "2d9e82263b94a343ee95d56c810a5a0adb63a439cd5b4944dfb56f09e28c6f04"
-    );
+  test("Dummy test", () => {
+    expect(true);
   });
 
-  test("Generate public key compressed", () => {
-    const pubKeyCompressed = privToPub(privKeyBuff, true);
-    expect(bytesToHex(pubKeyCompressed)).toEqual(
-      "0xad9e82263b94a343ee95d56c810a5a0adb63a439cd5b4944dfb56f09e28c6f04"
-    );
-  });
+  // const privKey =
+  //   "0x28156abe7fe2fd433dc9df969286b96666489bac508612d0e16593e944c4f69f";
+  // const privKeyBuff = hexToBytes(privKey);
 
-  test("Generate public key from private key different than 32 bytes", () => {
-    const privKey31 = Buffer.alloc(31).fill("A");
-    const privKey33 = Buffer.alloc(33).fill("B");
-    expect(() => {
-      privToPub(privKey31, true);
-    }).toThrow("Input Error: Buffer has 31 bytes. It should be 32 bytes");
-    expect(() => {
-      privToPub(privKey33, true);
-    }).toThrow("Input Error: Buffer has 33 bytes. It should be 32 bytes");
-  });
+  // test("Generate public key uncompressed", () => {
+  //   const pubKey = privToPub(privKeyBuff, false);
+  //   expect(bytesToHex(pubKey)).toEqual(
+  //     "0x270000b73fba5f79c0491a32d4e64f69813db369ea106c09bc5ca4ae220cbb81" +
+  //       "2d9e82263b94a343ee95d56c810a5a0adb63a439cd5b4944dfb56f09e28c6f04"
+  //   );
+  // });
 
-  test("Generate random private key, retrieve public", () => {
-    const randPrivKeyHex = genPriv();
-    const randPrivKeyBuff = hexToBytes(randPrivKeyHex);
-    expect(() => {
-      privToPub(randPrivKeyBuff, true);
-    }).not.toThrow();
-  });
+  // test("Generate public key compressed", () => {
+  //   const pubKeyCompressed = privToPub(privKeyBuff, true);
+  //   expect(bytesToHex(pubKeyCompressed)).toEqual(
+  //     "0xad9e82263b94a343ee95d56c810a5a0adb63a439cd5b4944dfb56f09e28c6f04"
+  //   );
+  // });
+
+  // test("Generate public key from private key different than 32 bytes", () => {
+  //   const privKey31 = Buffer.alloc(31).fill("A");
+  //   const privKey33 = Buffer.alloc(33).fill("B");
+  //   expect(() => {
+  //     privToPub(privKey31, true);
+  //   }).toThrow("Input Error: Buffer has 31 bytes. It should be 32 bytes");
+  //   expect(() => {
+  //     privToPub(privKey33, true);
+  //   }).toThrow("Input Error: Buffer has 33 bytes. It should be 32 bytes");
+  // });
+
+  // test("Generate random private key, retrieve public", () => {
+  //   const randPrivKeyHex = genPriv();
+  //   const randPrivKeyBuff = hexToBytes(randPrivKeyHex);
+  //   expect(() => {
+  //     privToPub(randPrivKeyBuff, true);
+  //   }).not.toThrow();
+  // });
 });
--- packages/sdk/src/crypto/babyjub-utils.ts
+++ packages/sdk/src/crypto/babyjub-utils.ts
@@ -13,83 +13,83 @@ import {
   bigIntToBufferBE,
 } from "../utils";
 
-/**
- * Get compressed point given a public key compsed by coordinate X and Y
- * @param {Buffer} pubKeyX - Coordinate X of public key
- * @param {Buffer} pubKeyY - Coordinate Y of public key
- * @returns {Buffer} - Public key compressed
- */
-export function compressPoint(pubKeyX: Buffer, pubKeyY: Buffer): Buffer {
-  const pubKeyXBigInt = bufferToBigIntBE(pubKeyX);
-  if (pubKeyXBigInt.greater(babyjub.p.shr(1))) {
-    pubKeyY[0] |= 0x80;
-  }
-  return pubKeyY;
-}
+// /**
+//  * Get compressed point given a public key compsed by coordinate X and Y
+//  * @param {Buffer} pubKeyX - Coordinate X of public key
+//  * @param {Buffer} pubKeyY - Coordinate Y of public key
+//  * @returns {Buffer} - Public key compressed
+//  */
+// export function compressPoint(pubKeyX: Buffer, pubKeyY: Buffer): Buffer {
+//   const pubKeyXBigInt = bufferToBigIntBE(pubKeyX);
+//   if (pubKeyXBigInt.greater(babyjub.p.shr(1))) {
+//     pubKeyY[0] |= 0x80;
+//   }
+//   return pubKeyY;
+// }
 
-/**
- * Get number of bits given a big integer
- * @param {bigInt} number - big integer
- * @returns {number} - number of bits necessary to represent big integer input
- */
-export function bigIntbits(number: BigInteger): number {
-  let numBits = 0;
-  while (!number.isZero()) {
-    number = number.shiftRight(1);
-    numBits += 1;
-  }
-  return numBits;
-}
+// /**
+//  * Get number of bits given a big integer
+//  * @param {bigInt} number - big integer
+//  * @returns {number} - number of bits necessary to represent big integer input
+//  */
+// export function bigIntbits(number: BigInteger): number {
+//   let numBits = 0;
+//   while (!number.isZero()) {
+//     number = number.shiftRight(1);
+//     numBits += 1;
+//   }
+//   return numBits;
+// }
 
-/**
- * Generates a random private key in a subgroup specified by the babyjub field
- * @returns {string} - Hexadecimal string
- */
-export function genPriv(): string {
-  const randBytes = crypto.randomBytes(Math.floor(256 / 8));
-  const randHex = bytesToHex(randBytes);
-  return randHex;
-}
+// /**
+//  * Generates a random private key in a subgroup specified by the babyjub field
+//  * @returns {string} - Hexadecimal string
+//  */
+// export function genPriv(): string {
+//   const randBytes = crypto.randomBytes(Math.floor(256 / 8));
+//   const randHex = bytesToHex(randBytes);
+//   return randHex;
+// }
 
-/**
- * Retrieve uniform scalar in babyjub curve subgroup
- * @param {Buffer} privKey - Private key
- * @returns {bigInt} scalar in subgroup babyjub order
- */
-export function privToScalar(privKey: Buffer): BigInteger {
-  const h1 = createBlakeHash("blake512").update(privKey).digest();
-  const sBuff = eddsa.pruneBuffer(h1.slice(0, 32));
-  const scalar = leBuffToInt(sBuff).shiftRight(3);
-  if (scalar >= babyjub.p) {
-    throw new Error("scalar generated larger than subgroup");
-  }
-  return scalar;
-}
+// /**
+//  * Retrieve uniform scalar in babyjub curve subgroup
+//  * @param {Buffer} privKey - Private key
+//  * @returns {bigInt} scalar in subgroup babyjub order
+//  */
+// export function privToScalar(privKey: Buffer): BigInteger {
+//   const h1 = createBlakeHash("blake512").update(privKey).digest();
+//   const sBuff = eddsa.pruneBuffer(h1.slice(0, 32));
+//   const scalar = leBuffToInt(sBuff).shiftRight(3);
+//   if (scalar >= babyjub.p) {
+//     throw new Error("scalar generated larger than subgroup");
+//   }
+//   return scalar;
+// }
 
-/**
- * Retrieve public key from private key in a babyjub curve
- * @param {Buffer} privKey - Private key
- * @param {bool} compress - Flag to indicate if output is public key compresed or not
- * @returns {Buffer} New public key generated
- */
-export function privToPub(privKey: Buffer, compress: boolean): Buffer {
-  if (privKey.length !== 32) {
-    throw new Error(
-      `Input Error: Buffer has ${privKey.length} bytes. It should be 32 bytes`
-    );
-  }
-  const scalar = privToScalar(privKey);
-  const pubKey = babyjub.mulPointEscalar(babyjub.Base8, scalar);
+// /**
+//  * Retrieve public key from private key in a babyjub curve
+//  * @param {Buffer} privKey - Private key
+//  * @param {bool} compress - Flag to indicate if output is public key compresed or not
+//  * @returns {Buffer} New public key generated
+//  */
+// export function privToPub(privKey: Buffer, compress: boolean): Buffer {
+//   if (privKey.length !== 32) {
+//     throw new Error(
+//       `Input Error: Buffer has ${privKey.length} bytes. It should be 32 bytes`
+//     );
+//   }
+//   const scalar = privToScalar(privKey);
+//   const pubKey = babyjub.mulPointEscalar(babyjub.Base8, scalar);
 
-  console.log("PUBKEY: ", pubKey);
+//   console.log("PUBKEY: ", pubKey);
 
-  const pubKeyX = bigIntToBufferBE(bigInt(pubKey[0]));
-  const pubKeyY = bigIntToBufferBE(bigInt(pubKey[1]));
-  if (!babyjub.inSubgroup(pubKey)) {
-    throw new Error("Point generated not in babyjub subgroup");
-  }
-  if (!compress) {
-    return Buffer.concat([pubKeyX, pubKeyY]);
-  }
-  return compressPoint(pubKeyX, pubKeyY);
-}
+//   const pubKeyX = bigIntToBufferBE(bigInt(pubKey[0]));
+//   const pubKeyY = bigIntToBufferBE(bigInt(pubKey[1]));
+//   if (!babyjub.inSubgroup(pubKey)) {
+//     throw new Error("Point generated not in babyjub subgroup");
+//   }
+//   if (!compress) {
+//     return Buffer.concat([pubKeyX, pubKeyY]);
+//   }
+//   return compressPoint(pubKeyX, pubKeyY);
+// }
