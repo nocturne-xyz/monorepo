5b1046e214fc544a1e1779851d7c15949eb73ea7
Luke Tchang <ltchang@stanford.edu>
2022-10-28 22:17:59 +0100

    feat(sdk): add proper indexing in chunks to merkle prover and notes manager

--- packages/sdk/src/sdk/notesManager.ts
+++ packages/sdk/src/sdk/notesManager.ts
@@ -1,39 +1,49 @@
 import { Wallet, Wallet__factory } from "@flax/contracts";
+import { RefundEvent } from "@flax/contracts/dist/src/Wallet";
 import { ethers } from "ethers";
 import { Address } from "../commonTypes";
 import { FlattenedFlaxAddress } from "../crypto/address";
 import { FlaxDB } from "./flaxDb";
 import { IncludedNote } from "./note";
+import { query } from "./utils";
 
-export abstract class NotesManager {
-  db: FlaxDB;
+const DEFAULT_START_BLOCK = 0;
+const REFUNDS_LAST_INDEXED_BLOCK = "REFUNDS_LAST_INDEXED_BLOCK";
 
-  constructor(db: FlaxDB) {
-    this.db = db;
-  }
-
-  abstract gatherNewRefunds(): Promise<IncludedNote[]>;
+export interface NotesManager {
+  gatherNewRefunds(): Promise<IncludedNote[]>;
   // TODO: abstract gatherNewSpends():
   // TODO: method to call two gather functions and update DB accordingly
 }
 
-export class ChainIndexingNotesManager extends NotesManager {
-  wallet: Wallet;
+export class ChainIndexingNotesManager implements NotesManager {
+  db: FlaxDB;
+  walletContract: Wallet;
+  provider: ethers.providers.Provider;
 
   constructor(walletAddress: Address, rpcUrl: string, db: FlaxDB) {
-    super(db);
-
-    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-    this.wallet = Wallet__factory.connect(walletAddress, provider);
+    this.db = db;
+    this.provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    this.walletContract = Wallet__factory.connect(walletAddress, this.provider);
   }
 
   async gatherNewRefunds(): Promise<IncludedNote[]> {
-    // TODO: load fromBlock and toBlock from FlaxDB
-    let fromBlock = 0;
-    let toBlock = 0;
+    let maybeLastSeen = this.db.getKv(REFUNDS_LAST_INDEXED_BLOCK);
+    const lastSeen = maybeLastSeen
+      ? parseInt(maybeLastSeen)
+      : DEFAULT_START_BLOCK; // TODO: load default from network-specific config
+    let latestBlock = await this.provider.getBlockNumber();
 
-    const filter = this.wallet.filters.Refund();
-    const events = await this.wallet.queryFilter(filter, fromBlock, toBlock);
+    const filter = this.walletContract.filters.Refund();
+    let events: RefundEvent[] = await query(
+      this.walletContract,
+      filter,
+      lastSeen,
+      latestBlock
+    );
+
+    events = events.sort((a, b) => a.blockNumber - b.blockNumber);
+
     return events.map((event) => {
       const { refundAddr, nonce, asset, id, value, merkleIndex } = event.args;
       const { h1X, h1Y, h2X, h2Y } = refundAddr;
