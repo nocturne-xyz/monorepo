c92229ea46eb1bd2ef4618596ff0e5ba5b4c7526
Luke Tchang <ltchang@stanford.edu>
2022-11-01 10:42:43 +0000

    fix(sdk): fix multiple val single key lmdb case

--- packages/sdk/src/sdk/db/lmdb.ts
+++ packages/sdk/src/sdk/db/lmdb.ts
@@ -1,51 +1,63 @@
-import findWorkspaceRoot from "find-yarn-workspace-root";
-import * as path from "path";
-import { open, RootDatabase } from "lmdb";
-import { FlaxDB, NOTES_PREFIX } from ".";
+import { open, RootDatabase, Database } from "lmdb";
+import { DEFAULT_DB_PATH, FlaxDB, NOTES_PREFIX } from ".";
 import { IncludedNoteStruct, includedNoteStructFromJSON } from "../note";
 import { Asset } from "../../commonTypes";
 
-const ROOT_DIR = findWorkspaceRoot()!;
-
 export class FlaxLMDB implements FlaxDB {
-  db: RootDatabase<string, string>;
+  rootDb: RootDatabase<string, string>;
+  kvDb: Database<string, string>;
+  notesDb: Database<string, string>;
 
-  constructor(dbPath?: string) {
-    if (!dbPath) {
-      dbPath = path.join(ROOT_DIR, "db");
-    }
-    this.db = open({ path: dbPath, dupSort: true, encoding: "ordered-binary" });
+  constructor(dbPath: string = DEFAULT_DB_PATH) {
+    this.rootDb = open({
+      path: dbPath,
+    });
+    this.kvDb = this.rootDb.openDB({ name: "kv" });
+    this.notesDb = this.rootDb.openDB({
+      name: "notes",
+      dupSort: true,
+      encoding: "ordered-binary",
+      sharedStructuresKey: Symbol.for(NOTES_PREFIX),
+    });
   }
 
   putKv(key: string, value: string): Promise<boolean> {
-    return this.db.put(key, value);
+    return this.kvDb.put(key, value);
   }
 
   getKv(key: string): string | undefined {
-    return this.db.get(key);
+    return this.kvDb.get(key);
   }
 
   storeNote(note: IncludedNoteStruct): Promise<boolean> {
     const asset = new Asset(note.asset, note.id);
     const key = FlaxDB.notesKey(asset);
-    return this.putKv(key, JSON.stringify(note));
+    return this.notesDb.put(key, JSON.stringify(note));
   }
 
   getAllNotes(): Map<string, IncludedNoteStruct[]> {
-    const keys = this.db.getKeys();
+    const keys = this.notesDb.getKeys();
 
     const notesMap: Map<string, IncludedNoteStruct[]> = new Map();
     for (const key of keys) {
       const notesArray: IncludedNoteStruct[] = [];
-      if (key.startsWith(NOTES_PREFIX)) {
-        for (const val of this.db.getValues(key)) {
-          const includedNote = includedNoteStructFromJSON(val);
-          notesArray.push(includedNote);
-        }
-        notesMap.set(key, notesArray);
+      for (const val of this.notesDb.getValues(key)) {
+        const includedNote = includedNoteStructFromJSON(val);
+        notesArray.push(includedNote);
       }
+      notesMap.set(key, notesArray);
     }
 
     return notesMap;
+  }
+
+  clear(): void {
+    this.kvDb.clearSync();
+    this.notesDb.clearSync();
+    this.rootDb.clearSync();
+  }
+
+  async close(): Promise<void> {
+    await this.rootDb.close();
   }
 }
