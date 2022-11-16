5f02c511ace9526052f461aea74e4807b16e0406
Luke Tchang <ltchang@stanford.edu>
2022-11-15 22:36:13 -0800

    fix(sdk/site): can instantiate flax context in utils/snap

---
+++ packages/site/gatsby-node.js
@@ -0,0 +1,18 @@
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      fallback: {
+        assert: require.resolve("assert/"),
+        crypto: require.resolve("crypto-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        stream: require.resolve("stream-browserify"),
+        path: require.resolve("path-browserify"),
+        constants: require.resolve("constants-browserify"),
+        fs: false,
+        readline: false,
+      },
+    },
+  });
+};
