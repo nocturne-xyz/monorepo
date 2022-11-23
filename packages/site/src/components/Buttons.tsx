ce909dac9baa3125ce81f77c281ef4d742e9bbfa
Luke Tchang <ltchang@stanford.edu>
2022-11-22 22:02:40 -0800

    prog(site/snap): snap outsources dummy proving to browser

--- packages/site/src/components/Buttons.tsx
+++ packages/site/src/components/Buttons.tsx
@@ -110,6 +110,10 @@ export const SyncLeavesButton = (props: ComponentProps<typeof Button>) => {
   return <Button {...props}>SyncLeaves</Button>;
 };
 
+export const ProveSpend2Button = (props: ComponentProps<typeof Button>) => {
+  return <Button {...props}>ProveSpend2</Button>;
+};
+
 export const ClearDbButton = (props: ComponentProps<typeof Button>) => {
   return <Button {...props}>ClearDb</Button>;
 };
