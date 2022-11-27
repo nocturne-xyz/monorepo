af8e37815b1cbe59941b580c1620be957bc702c1
Sladuca <sladuca777@gmail.com>
2022-11-27 15:17:15 -0500

    use gsed on macos

--- packages/circuits/scripts/fixSolidityVerifier.sh
+++ packages/circuits/scripts/fixSolidityVerifier.sh
@@ -8,15 +8,23 @@ FILE="$1"
 FILENAME="${FILE##*/}"
 VERIFIERNAME="${FILENAME%.*}"
 
+CMD="sed"
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo "macOS detected... using gsed instead"
+  CMD="gsed"
+fi
+
+
 echo "Post processing solidity vierfier at $FILE with verifier name $VERIFIERNAME.."
 
-sed -i \
+$CMD -i \
   '/^pragma/s/.*/pragma solidity ^0.8.2;/
   /^pragma/aimport {I'$VERIFIERNAME'} from "./interfaces/I'$VERIFIERNAME'.sol";
   s/contract Verifier/contract '$VERIFIERNAME' is I'$VERIFIERNAME'/
   s/public view returns (bool r)/public override view returns (bool r)/' "$FILE"
 
-sed -ni \
+$CMD -ni \
   '1,/^import/p
   /^import/aimport {Pairing} from "./libs/Pairing.sol";
   /contract/,$p' "$FILE"
