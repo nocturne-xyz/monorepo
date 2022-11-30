35494881c223b4187acdaf215df1aeb7c05d6383
Luke Tchang <ltchang@stanford.edu>
2022-11-30 14:11:27 -0800

    feat/fix(site/e2e-tests): change gen proof name to getSpendInputs and add depositFund script for setting manual test state

---
+++ packages/e2e-tests/scripts/depositFunds.ts
@@ -0,0 +1,29 @@
+import { setup } from "../deploy/deployNocturne";
+const hre = require("hardhat");
+import { SimpleERC20Token__factory } from "@nocturne-xyz/contracts";
+import { ERC20_ID } from "@nocturne-xyz/sdk";
+
+(async () => {
+  const { wallet, vault, nocturneContext } = await setup();
+  const nocturneAddress = nocturneContext.signer.address;
+  console.log("Test Nocturne signer privkey: ", nocturneContext.signer.privkey);
+
+  const [eoa] = await hre.ethers.getSigners();
+  const tokenFactory = new SimpleERC20Token__factory(eoa);
+  const token = await tokenFactory.deploy();
+  console.log("Token deployed at: ", token.address);
+
+  token.reserveTokens(eoa.address, 1000);
+  await token.connect(eoa).approve(vault.address, 200);
+
+  const amounts = [100n, 100n];
+  for (let i = 0; i < amounts.length; i++) {
+    await wallet.connect(eoa).depositFunds({
+      spender: eoa.address,
+      asset: token.address,
+      value: amounts[i],
+      id: ERC20_ID,
+      depositAddr: nocturneAddress.toStruct(),
+    });
+  }
+})();
