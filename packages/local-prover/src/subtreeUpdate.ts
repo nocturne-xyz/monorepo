9782a82930fbf7bb263f9b9b7d8350cd6019b00b
Sebastien La Duca <sladuca777@gmail.com>
2022-11-30 20:53:27 -0500

    fix indentation

--- packages/local-prover/src/subtreeUpdate.ts
+++ packages/local-prover/src/subtreeUpdate.ts
@@ -8,12 +8,12 @@ import { sha256 } from "js-sha256";
 import * as snarkjs from "snarkjs";
 
 export const localSubtreeUpdateProver: SubtreeUpdateProver = {
-	prove: (inputs: SubtreeUpdateInputSignals, wasmPath: string, zkeyPath: string) => {
-		return snarkjs.groth16.fullProve(inputs, wasmPath, zkeyPath);
-	},
-	verify: ({ proof, publicSignals }: SubtreeUpdateProofWithPublicSignals, vkey: any) => {
-		return snarkjs.groth16.verify(vkey, publicSignals, proof);
-	},
+  prove: (inputs: SubtreeUpdateInputSignals, wasmPath: string, zkeyPath: string) => {
+    return snarkjs.groth16.fullProve(inputs, wasmPath, zkeyPath);
+  },
+  verify: ({ proof, publicSignals }: SubtreeUpdateProofWithPublicSignals, vkey: any) => {
+    return snarkjs.groth16.verify(vkey, publicSignals, proof);
+  },
 };
 
 export function encodePathAndHash(idx: bigint, accumulatorHashHi: bigint): bigint {
@@ -32,17 +32,17 @@ export function encodePathAndHash(idx: bigint, accumulatorHashHi: bigint): bigin
 
 // returns merkle proof for the 0th element inserted into the tree in this batch
 export function applyBatchUpdateToTree(batch: (Note | bigint)[], tree: BinaryPoseidonTree): MerkleProof {
-	for (const noteOrCommitment of batch) {
-		if (typeof noteOrCommitment === "bigint") {
-			const nc = noteOrCommitment;
-			tree.insert(nc);
-		} else {
-			const note = noteOrCommitment;
-			tree.insert(note.toCommitment());
-		}
-	}
+  for (const noteOrCommitment of batch) {
+    if (typeof noteOrCommitment === "bigint") {
+      const nc = noteOrCommitment;
+      tree.insert(nc);
+    } else {
+      const note = noteOrCommitment;
+      tree.insert(note.toCommitment());
+    }
+  }
 
-	return tree.getProof(tree.count - batch.length);
+  return tree.getProof(tree.count - batch.length);
 }
 
 /* generates inputs for subtree update circuit 
@@ -50,40 +50,40 @@ export function applyBatchUpdateToTree(batch: (Note | bigint)[], tree: BinaryPos
  * @param merkle - proof to the leftmost element of the batch. Assumes the batch is the last batch to have been inserted in the tree.
 */
 export function subtreeUpdateInputsFromBatch(batch: (Note | bigint)[], merkleProof: MerkleProof): SubtreeUpdateInputSignals {
-	if (batch.length !== BinaryPoseidonTree.BATCH_SIZE) {
-		throw new Error(`\`batch.length\` ${BinaryPoseidonTree.BATCH_SIZE}, `);
-	}
+  if (batch.length !== BinaryPoseidonTree.BATCH_SIZE) {
+    throw new Error(`\`batch.length\` ${BinaryPoseidonTree.BATCH_SIZE}, `);
+  }
 
-	const ownerH1s: bigint[] = [];
-	const ownerH2s: bigint[] = [];
-	const nonces: bigint[] = [];
-	const assets: bigint[] = [];
-	const ids: bigint[] = [];
-	const values: bigint[] = [];
+  const ownerH1s: bigint[] = [];
+  const ownerH2s: bigint[] = [];
+  const nonces: bigint[] = [];
+  const assets: bigint[] = [];
+  const ids: bigint[] = [];
+  const values: bigint[] = [];
 
-	const accumulatorPreimage = [];
-	const leaves = [];
-	const bitmap: bigint[] = [];
+  const accumulatorPreimage = [];
+  const leaves = [];
+  const bitmap: bigint[] = [];
 
-	// note fields
-	for (const noteOrCommitment of batch) {
-		if (typeof noteOrCommitment === "bigint") {
-			const nc = noteOrCommitment;
-			accumulatorPreimage.push(...[...new Uint8Array(bigintToBuf(nc, true))]);
-			leaves.push(nc);
-			bitmap.push(0n);
+  // note fields
+  for (const noteOrCommitment of batch) {
+    if (typeof noteOrCommitment === "bigint") {
+      const nc = noteOrCommitment;
+      accumulatorPreimage.push(...[...new Uint8Array(bigintToBuf(nc, true))]);
+      leaves.push(nc);
+      bitmap.push(0n);
 
-			ownerH1s.push(0n);
-			ownerH2s.push(0n);
-			nonces.push(0n);
-			assets.push(0n);
-			ids.push(0n);
-			values.push(0n);
-		} else {
-			const note = noteOrCommitment;
-			accumulatorPreimage.push(...note.sha256());
-			leaves.push(note.toCommitment());
-			bitmap.push(1n);
+      ownerH1s.push(0n);
+      ownerH2s.push(0n);
+      nonces.push(0n);
+      assets.push(0n);
+      ids.push(0n);
+      values.push(0n);
+    } else {
+      const note = noteOrCommitment;
+      accumulatorPreimage.push(...note.sha256());
+      leaves.push(note.toCommitment());
+      bitmap.push(1n);
 
       ownerH1s.push(note.owner.h1X);
       ownerH2s.push(note.owner.h2X);
@@ -91,15 +91,15 @@ export function subtreeUpdateInputsFromBatch(batch: (Note | bigint)[], merklePro
       assets.push(BigInt(note.asset));
       ids.push(note.id);
       values.push(note.value);
-		}
-	}
+    }
+  }
 
   // accumulatorHash
   const accumulatorHashU256 = hexToBigint(sha256.hex(accumulatorPreimage));
   const [accumulatorHashHi, accumulatorHash] = bigInt256ToFieldElems(accumulatorHashU256);
 
   const siblings = merkleProof.siblings.slice(BinaryPoseidonTree.S).map(arr => arr[0]);
-	const idx = merkleProof.pathIndices.reduce((idx, bit) => (idx << 1n) | BigInt(bit), 0n);
+  const idx = merkleProof.pathIndices.reduce((idx, bit) => (idx << 1n) | BigInt(bit), 0n);
 
   // encodedPathAndHash
   const encodedPathAndHash = encodePathAndHash(BigInt(idx), accumulatorHashHi);
