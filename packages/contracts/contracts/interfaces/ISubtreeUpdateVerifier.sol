5ae601b1761c0edb66946a64dcc7c75c944e43ba
Sladuca <sladuca777@gmail.com>
2022-12-05 16:14:33 -0500

    add batch verifier interface to SubtreeUpdateVerifier

--- packages/contracts/contracts/interfaces/ISubtreeUpdateVerifier.sol
+++ packages/contracts/contracts/interfaces/ISubtreeUpdateVerifier.sol
@@ -1,9 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.5;
 
+import {IBatchVerifier} from "./IBatchVerifier.sol";
+
 /// @title Verifier interface.
 /// @dev Interface of Verifier contract.
-interface ISubtreeUpdateVerifier {
+interface ISubtreeUpdateVerifier is IBatchVerifier {
     function verifyProof(
         uint256[2] memory a,
         uint256[2][2] memory b,
