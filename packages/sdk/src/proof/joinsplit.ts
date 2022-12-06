694f25d1e5dbd5d8217d487bdc56ae30097d648e
Wei Dai <me@wdai.us>
2022-12-06 12:25:50 -0800

    Remove unnecessary normalization for circuit values

--- packages/sdk/src/proof/joinsplit.ts
+++ packages/sdk/src/proof/joinsplit.ts
@@ -1,5 +1,3 @@
-import { normalizeBigInt } from "./utils";
-import { NocturneAddress } from "../crypto/address";
 import { BaseProof, MerkleProofInput, NoteInput } from "./types";
 
 export interface JoinSplitProver {
@@ -67,75 +65,5 @@ export function joinSplitPublicSignalsFromArray(
     opDigest: publicSignals[6],
     asset: publicSignals[7],
     id: publicSignals[8],
-  };
-}
-
-function normalizeNocturneAddressInput(
-  nocturneAddressInput: NocturneAddress
-): NocturneAddress {
-  const { h1X, h1Y, h2X, h2Y } = nocturneAddressInput;
-  return {
-    h1X: normalizeBigInt(h1X),
-    h1Y: normalizeBigInt(h1Y),
-    h2X: normalizeBigInt(h2X),
-    h2Y: normalizeBigInt(h2Y),
-  };
-}
-
-function normalizeNoteInput(noteInput: NoteInput): NoteInput {
-  const { owner, nonce, asset, value, id } = noteInput;
-  return {
-    owner: normalizeNocturneAddressInput(owner),
-    nonce: normalizeBigInt(nonce),
-    asset: normalizeBigInt(asset),
-    value: normalizeBigInt(value),
-    id: normalizeBigInt(id),
-  };
-}
-
-function normalizeMerkleProofInput(
-  merkleProofInput: MerkleProofInput
-): MerkleProofInput {
-  const { path, siblings } = merkleProofInput;
-  for (let i = 0; i < path.length; i++) {
-    path[i] = normalizeBigInt(path[i]);
-  }
-  for (let i = 0; i < siblings.length; i++) {
-    siblings[i] = normalizeBigInt(siblings[i]);
-  }
-
-  return { path, siblings };
-}
-
-export function normalizeJoinSplitInputs(
-  inputs: JoinSplitInputs
-): JoinSplitInputs {
-  const {
-    vk,
-    operationDigest,
-    oldNoteA,
-    oldNoteB,
-    spendPk,
-    newNoteA,
-    newNoteB,
-    merkleProofA,
-    merkleProofB,
-    c,
-    z,
-  } = inputs;
-  const [spendPkX, spendPkY] = spendPk;
-
-  return {
-    vk: normalizeBigInt(vk),
-    operationDigest: normalizeBigInt(operationDigest),
-    oldNoteA: normalizeNoteInput(oldNoteA),
-    oldNoteB: normalizeNoteInput(oldNoteB),
-    spendPk: [normalizeBigInt(spendPkX), normalizeBigInt(spendPkY)],
-    newNoteA: normalizeNoteInput(newNoteA),
-    newNoteB: normalizeNoteInput(newNoteB),
-    merkleProofA: normalizeMerkleProofInput(merkleProofA),
-    merkleProofB: normalizeMerkleProofInput(merkleProofB),
-    c: normalizeBigInt(c),
-    z: normalizeBigInt(z),
   };
 }
