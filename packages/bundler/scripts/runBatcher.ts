692f52e1520bc66f89fbffb8030a2236d482114d
Luke Tchang <ltchang@stanford.edu>
2022-12-20 15:04:54 -0800

    fix(bundler): delete scripts

--- packages/bundler/scripts/runBatcher.ts
+++
@@ -1,8 +0,0 @@
-import { BundlerBatcher } from "../src/batcher";
-import * as dotenv from "dotenv";
-
-(async () => {
-  dotenv.config();
-  const batcher = new BundlerBatcher(30, 8);
-  await batcher.run();
-})();
