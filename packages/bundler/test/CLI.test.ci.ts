41b2dd437142099eab773ea957d106b50eac2ab8
Luke Tchang <ltchang@stanford.edu>
2022-12-26 19:24:31 -0800

    fix(bundler): cli tests check stdout values

--- packages/bundler/test/CLI.test.ci.ts
+++ packages/bundler/test/CLI.test.ci.ts
@@ -1,9 +1,8 @@
 import { expect } from "chai";
-import { exec, ExecException } from "child_process";
+import { spawn } from "child_process";
 import findWorkspaceRoot from "find-yarn-workspace-root";
 
 import RedisMemoryServer from "redis-memory-server";
-import { sleep } from "../src/utils";
 
 const PORT = 3000;
 const WALLET_ADDRESS = "0xE706317bf66b1C741CfCa5dCf5B78A44B5eD79e0";
@@ -24,37 +23,51 @@ describe("Bundler CLI", async () => {
       "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
   });
 
-  it("`run server` command succeeds", async () => {
-    let error: ExecException | null | undefined;
-    exec(
-      `npx ts-node ${BUNDLER_CLI_PATH} run server --wallet-address ${WALLET_ADDRESS} --port ${PORT}`,
-      (err) => {
-        error = err;
-      }
-    );
+  async function executeCmdAndGetStdout(cmdArray: any[]): Promise<string> {
+    return await new Promise<string>((resolve) => {
+      const child = spawn(cmdArray[0], cmdArray.slice(1), { shell: true });
+      child.stdout.on("data", (data) => {
+        resolve(data.toString());
+      });
+    });
+  }
 
-    await sleep(15000);
-    console.log(error);
-    expect(error!).to.be.undefined;
+  it("`run server` command succeeds", async () => {
+    const stdout = await executeCmdAndGetStdout([
+      `npx`,
+      `ts-node`,
+      `${BUNDLER_CLI_PATH}`,
+      `run`,
+      `server`,
+      `--wallet-address`,
+      `${WALLET_ADDRESS}`,
+      `--port`,
+      `${PORT}`,
+    ]);
+    expect(stdout.includes("Bundler server listening")).to.be.true;
   });
 
   it("`run batcher` command succeeds", async () => {
-    let error: ExecException | null | undefined;
-    exec(`npx ts-node ${BUNDLER_CLI_PATH} run batcher`, (err) => {
-      error = err;
-    });
-
-    await sleep(15000);
-    expect(error!).to.be.undefined;
+    const stdout = await executeCmdAndGetStdout([
+      `npx`,
+      `ts-node`,
+      `${BUNDLER_CLI_PATH}`,
+      `run`,
+      `batcher`,
+    ]);
+    expect(stdout.includes("Batcher running")).to.be.true;
   });
 
   it("`run submitter` command succeeds", async () => {
-    let error: ExecException | null | undefined;
-    exec(`npx ts-node ${BUNDLER_CLI_PATH} run batcher`, (err) => {
-      error = err;
-    });
-
-    await sleep(15000);
-    expect(error!).to.be.undefined;
+    const stdout = await executeCmdAndGetStdout([
+      `npx`,
+      `ts-node`,
+      `${BUNDLER_CLI_PATH}`,
+      `run`,
+      `submitter`,
+      `--wallet-address`,
+      `${WALLET_ADDRESS}`,
+    ]);
+    expect(stdout.includes("Submitter running")).to.be.true;
   });
 });
