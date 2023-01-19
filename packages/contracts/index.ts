ef8753636ac6075d9db9060607ef614342f3a7f8
Luke Tchang <ltchang@stanford.edu>
2023-01-18 21:37:25 -0800

    prog(contracts): make vault upgradeable too

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -1,33 +1,41 @@
 /* tslint:disable */
 /* eslint-disable */
-export type { Wallet } from "./src/Wallet";
-export type { Vault } from "./src/Vault";
-export type { JoinSplitVerifier } from "./src/JoinSplitVerifier";
+export type { Wallet } from './src/Wallet';
+export type { Vault } from './src/Vault';
+export type { JoinSplitVerifier } from './src/JoinSplitVerifier';
 export type { SubtreeUpdateVerifier } from './src/SubtreeUpdateVerifier';
 export type { TestSubtreeUpdateVerifier } from './src/TestSubtreeUpdateVerifier';
-export type { CommitmentTreeManager } from "./src/CommitmentTreeManager";
-export type { PoseidonHasherT3 } from "./src/PoseidonHasherT3";
-export type { PoseidonHasherT4 } from "./src/PoseidonHasherT4";
-export type { PoseidonHasherT5 } from "./src/PoseidonHasherT5";
-export type { PoseidonHasherT6 } from "./src/PoseidonHasherT6";
+export type { CommitmentTreeManager } from './src/CommitmentTreeManager';
+export type { PoseidonHasherT3 } from './src/PoseidonHasherT3';
+export type { PoseidonHasherT4 } from './src/PoseidonHasherT4';
+export type { PoseidonHasherT5 } from './src/PoseidonHasherT5';
+export type { PoseidonHasherT6 } from './src/PoseidonHasherT6';
 
-export type { IERC20Interface } from "./src/IERC20";
-export type { IERC721Interface } from "./src/IERC721";
-export type { IERC1155Interface } from "./src/IERC1155";
+export type { IERC20Interface } from './src/IERC20';
+export type { IERC721Interface } from './src/IERC721';
+export type { IERC1155Interface } from './src/IERC1155';
 
-export { Wallet__factory } from "./src/factories/Wallet__factory";
-export { Vault__factory } from "./src/factories/Vault__factory";
-export { JoinSplitVerifier__factory } from "./src/factories/JoinSplitVerifier__factory";
-export { SubtreeUpdateVerifier__factory } from "./src/factories/SubtreeUpdateVerifier__factory";
-export { TestSubtreeUpdateVerifier__factory } from "./src/factories/TestSubtreeUpdateVerifier__factory";
-export { CommitmentTreeManager__factory } from "./src/factories/CommitmentTreeManager__factory";
-export { PoseidonHasherT3__factory } from "./src/factories/PoseidonHasherT3__factory";
-export { PoseidonHasherT4__factory } from "./src/factories/PoseidonHasherT4__factory";
-export { PoseidonHasherT5__factory } from "./src/factories/PoseidonHasherT5__factory";
-export { PoseidonHasherT6__factory } from "./src/factories/PoseidonHasherT6__factory";
+export type { TransparentUpgradeableProxy } from './src/TransparentUpgradeableProxy';
+export type { ProxyAdmin } from './src/ProxyAdmin';
+export type { Versioned } from './src/Versioned';
 
-export { SimpleERC20Token__factory } from "./src/factories/SimpleERC20Token__factory";
-export { SimpleERC721Token__factory } from "./src/factories/SimpleERC721Token__factory";
-export { SimpleERC1155Token__factory } from "./src/factories/SimpleERC1155Token__factory";
+export { Wallet__factory } from './src/factories/Wallet__factory';
+export { Vault__factory } from './src/factories/Vault__factory';
+export { JoinSplitVerifier__factory } from './src/factories/JoinSplitVerifier__factory';
+export { SubtreeUpdateVerifier__factory } from './src/factories/SubtreeUpdateVerifier__factory';
+export { TestSubtreeUpdateVerifier__factory } from './src/factories/TestSubtreeUpdateVerifier__factory';
+export { CommitmentTreeManager__factory } from './src/factories/CommitmentTreeManager__factory';
+export { PoseidonHasherT3__factory } from './src/factories/PoseidonHasherT3__factory';
+export { PoseidonHasherT4__factory } from './src/factories/PoseidonHasherT4__factory';
+export { PoseidonHasherT5__factory } from './src/factories/PoseidonHasherT5__factory';
+export { PoseidonHasherT6__factory } from './src/factories/PoseidonHasherT6__factory';
 
-export { version } from "./package.json";
+export { SimpleERC20Token__factory } from './src/factories/SimpleERC20Token__factory';
+export { SimpleERC721Token__factory } from './src/factories/SimpleERC721Token__factory';
+export { SimpleERC1155Token__factory } from './src/factories/SimpleERC1155Token__factory';
+
+export { TransparentUpgradeableProxy__factory } from './src/factories/TransparentUpgradeableProxy__factory';
+export { ProxyAdmin__factory } from './src/factories/ProxyAdmin__factory';
+export { Versioned__factory } from './src/factories/Versioned__factory';
+
+export { version } from './package.json';
