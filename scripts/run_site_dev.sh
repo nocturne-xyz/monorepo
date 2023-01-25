6f031e077c32bd244403c189c1aa6117ee5c5089
Sladuca <sladuca777@gmail.com>
2023-01-25 13:37:48 -0500

    add comment

--- scripts/run_site_dev.sh
+++ scripts/run_site_dev.sh
@@ -41,6 +41,7 @@ sleep 3
 echo "Running deposit funds script..."
 yarn hh-node-deposit &> "$LOG_DIR/hh-node-deposit" || { echo 'hh-node-deposit failed' ; exit 1; }
 
+# read config variables from logs
 read WALLET_ADDRESS < <(sed -nr 's/deploying "Wallet_Proxy" \(tx: 0x[0-9a-fA-F]+\)\.\.\.: deployed at (0x[0-9a-fA-F]+) with [0-9]+ gas/\1/p' $LOG_DIR/hh-node)
 read TOKEN_CONTRACT_ADDR1 < <(sed -nr 's/^Token 1 deployed at: (0x[a-fA-F0-9]{40})$/\1/p' $LOG_DIR/hh-node-deposit)
 read TOKEN_CONTRACT_ADDR2 < <(sed -nr 's/^Token 2 deployed at: (0x[a-fA-F0-9]{40})$/\1/p' $LOG_DIR/hh-node-deposit)
@@ -48,6 +49,7 @@ read BUNDLER_SUBMITTER_PRIVATE_KEY< <(grep -A1 "Account #15" $LOG_DIR/hh-node | 
 read SUBTREE_UPDATER_SUBMITTER_PRIVATE_KEY< <(grep -A1 "Account #16" $LOG_DIR/hh-node | grep "Private Key:" | sed -nr 's/Private Key: (0x[0-9a-fA-F]+)/\1/p')
 popd
 
+# bundler default config variables
 REDIS_URL="redis://redis:6379"
 REDIS_PASSWORD="baka"
 RPC_URL="http://host.docker.internal:8545"
