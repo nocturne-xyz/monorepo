5734be5775c57fc15e3a88b5a9962ca501b17abf
Luke Tchang <ltchang@stanford.edu>
2023-01-24 17:43:17 -0800

    fix(frontend-sdk): balance display uses table instead

--- packages/frontend-sdk/src/components/BalanceDisplay.tsx
+++ packages/frontend-sdk/src/components/BalanceDisplay.tsx
@@ -6,59 +6,64 @@ interface AssetBalancesDisplayProps {
   frontendSDK?: NocturneFrontendSDK;
 }
 
+interface AbbreviatedAssetWithBalance extends AssetWithBalance {
+  abbreviatedAddress: string;
+}
+
 export const AssetBalancesDisplay: React.FC<AssetBalancesDisplayProps> = ({
   frontendSDK,
 }) => {
-  const [balances, setBalances] = useState<AssetWithBalance[]>([]);
+  const [balances, setBalances] = useState<AbbreviatedAssetWithBalance[]>([]);
+
+  const fetchData = async () => {
+    if (!frontendSDK) return;
+
+    console.log("Syncing snap balances...");
+    const data = await frontendSDK.getAllBalances();
+    const abbreviated = data.map(({ asset, balance }) => {
+      const { assetAddr } = asset;
+      const abbreviatedAddress =
+        assetAddr.substring(0, 6) +
+        "..." +
+        assetAddr.substring(assetAddr.length - 4);
+      return { asset, balance, abbreviatedAddress: abbreviatedAddress };
+    });
+    setBalances(abbreviated);
+  };
+
+  const handleClick = (address: string) => {
+    navigator.clipboard.writeText(address);
+  };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (!frontendSDK) return;
-
-      console.log("Syncing snap balances...");
-      frontendSDK
-        .getAllBalances()
-        .then((response) => {
-          console.log("Balances: ", response);
-          setBalances(response);
-        })
-        .catch((error) => console.log(error));
+      fetchData();
     }, 5000);
-
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div>
-      <div>
-        {balances.map(({ asset, balance }, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                backgroundColor: "#505050",
-                color: "white",
-                overflowWrap: "break-word",
-                padding: "5px",
-              }}
+    <table>
+      <thead>
+        <tr>
+          <th style={{ textAlign: "left" }}>Address</th>
+          <th style={{ textAlign: "left" }}>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {balances.map((balance, index) => (
+          <tr key={index}>
+            <td
+              style={{ textAlign: "left", color: "#ADD8E6", cursor: "pointer" }}
+              onClick={() => handleClick(balance.asset.assetAddr.toLowerCase())}
             >
-              <div>{`Address: ${asset.assetAddr}`}</div>
-              <div>{`Balance: ${balance}`}</div>
-              {index !== balances.length - 1 && (
-                <div
-                  style={{
-                    height: "1px",
-                    width: "100%",
-                    backgroundColor: "white",
-                    margin: "5px 0 0 0",
-                  }}
-                />
-              )}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+              {balance.abbreviatedAddress.toLowerCase()}
+            </td>
+            <td style={{ textAlign: "left" }}>{balance.balance.toString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
