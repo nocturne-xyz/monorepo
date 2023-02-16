0e2bce9d92d51c6893a56ed743bf3b09547f3d9f
Sladuca <sladuca777@gmail.com>
2023-02-16 17:34:55 -0500

    clean exports at sdk root

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -1,11 +1,9 @@
-export * from "./src/contract/types";
-export * from "./src/commonTypes";
 export * from "./src/sdk";
-export * from "./src/sdk/utils";
 export * from "./src/crypto";
 export * from "./src/proof";
 export * from "./src/contract";
-export * from "./src/primitives";
 export * from "./src/indexing";
 
-export * from "./src/NocturneContext";
+export * from "./src/commonTypes";
+export { BinaryPoseidonTree } from "./src/binaryPoseidonTree";
+export { NocturneContext } from "./src/NocturneContext";
