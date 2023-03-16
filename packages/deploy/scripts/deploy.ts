6026b0e38d35dd4b4694c5e454e5644334bc09c7
Luke Tchang <ltchang@stanford.edu>
2023-03-15 22:18:04 -0700

    fix(deploy): fix deploy script

--- packages/deploy/scripts/deploy.ts
+++ packages/deploy/scripts/deploy.ts
@@ -1,49 +1,68 @@
-// import { ethers } from "ethers";
-// import { deployNocturne } from "../src/deploy";
-// import * as dotenv from "dotenv";
-// import * as fs from "fs";
-// import { checkNocturneContractDeployment } from "../src/checks";
+import { ethers } from "ethers";
+import { deployNocturne } from "../src/deploy";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+import { checkNocturneContractDeployment } from "../src/checks";
 
-// const DEPLOYS_DIR = `${__dirname}/../deploys/`;
+const DEPLOYS_DIR = `${__dirname}/../deploys/`;
 
-// dotenv.config();
+dotenv.config();
 
-// (async () => {
-//   const proxyAdminOwner = process.env.PROXY_ADMIN_OWNER;
-//   if (!proxyAdminOwner) throw new Error("Missing proxy admin owner address");
+(async () => {
+  const proxyAdminOwner = process.env.PROXY_ADMIN_OWNER;
+  if (!proxyAdminOwner) throw new Error("Missing PROXY_ADMIN_OWNER");
 
-//   const useMockSubtreeUpdateVerifier =
-//     process.env.USE_MOCK_SUBTREE_UPDATE_VERIFIER != undefined;
+  const walletOwner = process.env.WALLET_OWNER;
+  if (!walletOwner) throw new Error("Missing WALLET_OWNER");
 
-//   const deployerKey = process.env.DEPLOYER_KEY;
-//   if (!deployerKey) throw new Error("Missing deployer key");
+  const depositManagerOwner = process.env.DEPOSIT_MANAGER_OWNER;
+  if (!depositManagerOwner) throw new Error("Missing DEPOSIT_MANAGER_OWNER");
 
-//   const rpcUrl = process.env.RPC_URL;
-//   if (!rpcUrl) throw new Error("Missing rpc url");
+  const screenersString = process.env.SCREENERS;
+  if (!screenersString) throw new Error("Missing SCREENERS");
+  const screeners = screenersString?.split(",");
 
-//   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-//   const deployer = new ethers.Wallet(deployerKey, provider);
+  const useMockSubtreeUpdateVerifier =
+    process.env.USE_MOCK_SUBTREE_UPDATE_VERIFIER != undefined;
 
-//   const deployment = await deployNocturne(proxyAdminOwner, {
-//     useMockSubtreeUpdateVerifier,
-//   });
+  const deployerKey = process.env.DEPLOYER_KEY;
+  if (!deployerKey) throw new Error("Missing DEPLOYER_KEY");
 
-//   console.log(deployment);
+  const rpcUrl = process.env.RPC_URL;
+  if (!rpcUrl) throw new Error("Missing RPC_URL");
 
-//   await checkNocturneContractDeployment(deployment, provider);
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const deployer = new ethers.Wallet(deployerKey, provider);
 
-//   if (!fs.existsSync(DEPLOYS_DIR)) {
-//     fs.mkdirSync(DEPLOYS_DIR);
-//   }
+  const deployment = await deployNocturne(
+    {
+      connectedSigner: deployer,
+      proxyAdminOwner,
+      walletOwner,
+      depositManagerOwner,
+      screeners,
+    },
+    {
+      useMockSubtreeUpdateVerifier,
+    }
+  );
 
-//   fs.writeFileSync(
-//     `${DEPLOYS_DIR}/${deployment.network.name}-${Date.now().toString()}.json`,
-//     JSON.stringify(deployment),
-//     {
-//       encoding: "utf8",
-//       flag: "w",
-//     }
-//   );
+  console.log(deployment);
 
-//   process.exit(0);
-// })();
+  await checkNocturneContractDeployment(deployment, provider);
+
+  if (!fs.existsSync(DEPLOYS_DIR)) {
+    fs.mkdirSync(DEPLOYS_DIR);
+  }
+
+  fs.writeFileSync(
+    `${DEPLOYS_DIR}/${deployment.network.name}-${Date.now().toString()}.json`,
+    JSON.stringify(deployment),
+    {
+      encoding: "utf8",
+      flag: "w",
+    }
+  );
+
+  process.exit(0);
+})();
