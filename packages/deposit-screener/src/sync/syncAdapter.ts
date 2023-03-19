8da8fa78318e0192e9d63eab16c1e7d92f926950
Luke Tchang <ltchang@stanford.edu>
2023-03-19 16:27:41 -0700

    prog(deposit-screener): add processor impl

--- packages/deposit-screener/src/sync/syncAdapter.ts
+++ packages/deposit-screener/src/sync/syncAdapter.ts
@@ -1,10 +1,15 @@
 import { ClosableAsyncIterator, IterSyncOpts } from "@nocturne-xyz/sdk";
 import { DepositEvent, DepositEventType } from "../types";
 
+export interface DepositEventsBatch {
+  blockNumber: number;
+  depositEvents: DepositEvent[];
+}
+
 export interface ScreenerSyncAdapter {
   iterDepositEvents(
     type: DepositEventType,
     startBlock: number,
     opts?: IterSyncOpts
-  ): ClosableAsyncIterator<DepositEvent[]>;
+  ): ClosableAsyncIterator<DepositEventsBatch>;
 }
