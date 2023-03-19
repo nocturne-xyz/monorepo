448f2e43ca723436e7130be155a7008674ef48c3
Luke Tchang <ltchang@stanford.edu>
2023-03-19 13:16:26 -0700

    prog(deposit-screener): add interfaces and dir structure for handling and screening deposit requests, TODO function that updates screener db after handle

---
+++ packages/deposit-screener/src/db/index.ts
@@ -0,0 +1,1 @@
+export { RateLimitDB } from "./ratelimitdb";
---
+++ packages/deposit-screener/src/delay/index.ts
@@ -0,0 +1,9 @@
+import { DepositRequest } from "@nocturne-xyz/sdk";
+
+// TODO: add real implementation
+export async function calculateDelaySeconds(
+  depositRequest: DepositRequest
+): Promise<number> {
+  depositRequest;
+  return 0;
+}
