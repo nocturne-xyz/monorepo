11c1fdc0c5937b16ab60956bacb7068bd0037ec9
Sladuca <sladuca777@gmail.com>
2023-03-21 10:12:46 -0400

    hardcode subtree updater address in site script

--- scripts/run_site_dev.sh
+++ scripts/run_site_dev.sh
@@ -44,12 +44,18 @@ yarn graph-node &> "$LOG_DIR/graph-node" &
 START_BLOCK=0
 BUNDLER_TX_SIGNER_KEY="0x0000000000000000000000000000000000000000000000000000000000000004"
 SUBTREE_UPDATER_TX_SIGNER_KEY="0x0000000000000000000000000000000000000000000000000000000000000005"
+# ran the following script to get this:
+# import { ethers } from "ethers";
+# const sk = "0x0000000000000000000000000000000000000000000000000000000000000005";
+# const signer = new ethers.Wallet(sk)
+# console.log(signer.address);
+SUBTREE_UPDATER_ADDRESS="0xe1AB8145F7E55DC933d51a18c793F901A3A0b276"
 
 # Eth address: 0xE57bFE9F44b819898F47BF37E5AF72a0783e1141
 SCREENER_TX_SIGNER_KEY="0x0000000000000000000000000000000000000000000000000000000000000006"
 # deposit
 echo "Running deposit funds script..."
-SUBTREE_BATCH_FILLER="$SUBTREE_UPDATER_TX_SIGNER_KEY" yarn hh-node-deposit &> "$LOG_DIR/hh-node-deposit" || { echo 'hh-node-deposit failed' ; exit 1; }
+SUBTREE_BATCH_FILLER="$SUBTREE_UPDATER_ADDRESS" yarn hh-node-deposit &> "$LOG_DIR/hh-node-deposit" || { echo 'hh-node-deposit failed' ; exit 1; }
 
 # read config variables from logs
 read DEPOSIT_MANAGER_CONTRACT_ADDRESS < <(sed -nr 's/^DepositManager address: (0x[a-fA-F0-9]{40})$/\1/p' $LOG_DIR/hh-node-deposit)
