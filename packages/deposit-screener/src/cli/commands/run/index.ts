105e50d2bc4e66886cee513fb5c7f1581d6ce004
Luke Tchang <ltchang@stanford.edu>
2023-03-20 22:34:10 -0700

    fix(deposit-screener): changelogs + cli processor rename

--- packages/deposit-screener/src/cli/commands/run/index.ts
+++ packages/deposit-screener/src/cli/commands/run/index.ts
@@ -1,9 +1,9 @@
 import { Command } from "commander";
-import runProcess from "./process";
+import runProcessor from "./processor";
 
 const run = new Command("run").description(
   "Run a deposit screener action (run server or process)"
 );
-run.addCommand(runProcess);
+run.addCommand(runProcessor);
 
 export default run;
---
+++ packages/deposit-screener/src/cli/commands/run/screener.ts
@@ -0,0 +1,49 @@
+import { Command } from "commander";
+import { ethers } from "ethers";
+import { DepositScreenerProcessor } from "../../../processor";
+import { SubgraphScreenerSyncAdapter } from "../../../sync/subgraph/adapter";
+import { getRedis } from "../utils";
+
+const runProcessor = new Command("screener")
+  .summary("Process deposit requests")
+  .description(
+    "Must supply .env file with REDIS_URL, RPC_URL, TX_SIGNER_KEY, and SUBGRAPH_URL. Must supply deposit manager contract address as options."
+  )
+  .requiredOption(
+    "--deposit-manager-address <string>",
+    "deposit manager contract address"
+  )
+  .action(async (options) => {
+    const { depositManagerAddress } = options;
+
+    // TODO: enable switching on adapter impl
+    const subgraphEndpoint = process.env.SUBGRAPH_URL;
+    if (!subgraphEndpoint) {
+      throw new Error("Missing SUBGRAPH_URL");
+    }
+    const adapter = new SubgraphScreenerSyncAdapter(subgraphEndpoint);
+
+    const rpcUrl = process.env.RPC_URL;
+    if (!rpcUrl) {
+      throw new Error("Missing RPC_URL");
+    }
+
+    const privateKey = process.env.TX_SIGNER_KEY;
+    if (!privateKey) {
+      throw new Error("Missing TX_SIGNER_KEY");
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    const signingProvider = new ethers.Wallet(privateKey, provider);
+
+    const processor = new DepositScreenerProcessor(
+      adapter,
+      depositManagerAddress,
+      signingProvider,
+      getRedis()
+    );
+
+    await processor.run();
+  });
+
+export default runProcessor;
