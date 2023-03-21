9459c80ad9444de78a82a1d7d2efbbb3540e94d5
Luke Tchang <ltchang@stanford.edu>
2023-03-20 20:44:43 -0700

    fix(deposit-screener): move redis env stuff to cli, remove submitter

--- packages/deposit-screener/src/processor.ts
+++ packages/deposit-screener/src/processor.ts
@@ -2,8 +2,12 @@ import {
   DepositManager,
   DepositManager__factory,
 } from "@nocturne-xyz/contracts";
-import { Address, DepositRequest } from "@nocturne-xyz/sdk";
-import { Queue } from "bullmq";
+import {
+  Address,
+  DepositRequest,
+  parseEventsFromContractReceipt,
+} from "@nocturne-xyz/sdk";
+import { Job, Queue, Worker } from "bullmq";
 import { ethers } from "ethers";
 import { checkDepositRequest } from "./check";
 import { DepositScreenerDB } from "./db";
@@ -14,41 +18,60 @@ import {
   DepositEventType,
   DepositRequestStatus,
   DELAYED_DEPOSIT_QUEUE,
+  DelayedDepositJobData,
 } from "./types";
-import { getRedis } from "./utils";
 import IORedis from "ioredis";
 import { DelayCalculator, DummyDelayCalculator } from "./delay";
+import {
+  EIP712Domain,
+  hashDepositRequest,
+  signDepositRequest,
+} from "./typedData";
+import { assert } from "console";
+import {
+  DEPOSIT_MANAGER_CONTRACT_NAME,
+  DEPOSIT_MANAGER_CONTRACT_VERSION,
+} from "./typedData/constants";
+import { DepositCompletedEvent } from "@nocturne-xyz/contracts/dist/src/DepositManager";
 
 export class DepositScreenerProcessor {
   adapter: ScreenerSyncAdapter;
   depositManagerContract: DepositManager;
   screeningApi: ScreeningApi;
-  db: DepositScreenerDB;
   delayCalculator: DelayCalculator;
   delayQueue: Queue;
+  db: DepositScreenerDB;
+  redis: IORedis;
+  signingProvider: ethers.Wallet; // ethers.Wallet for signing typed data
 
   constructor(
     syncAdapter: ScreenerSyncAdapter,
     depositManagerAddress: Address,
-    provider: ethers.providers.Provider,
-    redis?: IORedis
+    signingProvider: ethers.Wallet,
+    redis: IORedis
   ) {
+    this.redis = redis;
     this.adapter = syncAdapter;
+
+    this.signingProvider = signingProvider;
 
     this.depositManagerContract = DepositManager__factory.connect(
       depositManagerAddress,
-      provider
+      signingProvider.provider
     );
 
-    const connection = getRedis(redis);
-    this.db = new DepositScreenerDB(connection);
-    this.delayQueue = new Queue(DELAYED_DEPOSIT_QUEUE, { connection });
+    this.db = new DepositScreenerDB(redis);
+    this.delayQueue = new Queue(DELAYED_DEPOSIT_QUEUE, { connection: redis });
 
     this.screeningApi = new DummyScreeningApi();
     this.delayCalculator = new DummyDelayCalculator();
   }
 
   async run(): Promise<void> {
+    await Promise.all([this.runScreener(), this.runSubmitter()]);
+  }
+
+  async runScreener(): Promise<void> {
     const nextBlockToSync = await this.db.getNextBlock();
     console.log(
       `processing deposit requests starting from block ${nextBlockToSync}`
@@ -79,6 +102,92 @@ export class DepositScreenerProcessor {
       return DepositRequestStatus.Enqueued;
     } else {
       return status;
+    }
+  }
+
+  async runSubmitter(): Promise<void> {
+    const worker = new Worker(
+      DELAYED_DEPOSIT_QUEUE,
+      async (job: Job<DelayedDepositJobData>) => {
+        const depositRequest: DepositRequest = JSON.parse(
+          job.data.depositRequestJson
+        );
+
+        const hash = hashDepositRequest(depositRequest);
+        const inSet =
+          await this.depositManagerContract._outstandingDepositHashes(hash);
+        if (!inSet) {
+          console.log(
+            `Deposit already retrieved or completed. Spender: ${depositRequest.spender}. Nonce: ${depositRequest.nonce}`
+          );
+          return; // Already retrieved or completed
+        }
+
+        const valid = this.screeningApi.validDepositRequest(depositRequest);
+        if (!valid) {
+          console.log(
+            `Deposit no longer passes screening. Spender: ${depositRequest.spender}. Nonce: ${depositRequest.nonce}`
+          );
+          return;
+        }
+
+        await this.signAndSubmitDeposit(depositRequest).catch((e) => {
+          throw new Error(e);
+        });
+      },
+      { connection: this.redis, autorun: false }
+    );
+
+    console.log(
+      `Submitter running. DepositManager contract: ${this.depositManagerContract.address}.`
+    );
+    await worker.run();
+  }
+
+  async signAndSubmitDeposit(depositRequest: DepositRequest): Promise<void> {
+    const chainId = BigInt(await this.signingProvider.getChainId());
+    assert(
+      chainId == depositRequest.chainId,
+      "connected chainId != deposit.chainId"
+    ); // Should never happen?
+
+    const domain: EIP712Domain = {
+      name: DEPOSIT_MANAGER_CONTRACT_NAME,
+      version: DEPOSIT_MANAGER_CONTRACT_VERSION,
+      chainId,
+      verifyingContract: this.depositManagerContract.address,
+    };
+    const signature = await signDepositRequest(
+      this.signingProvider,
+      domain,
+      depositRequest
+    );
+
+    const tx = await this.depositManagerContract.completeDeposit(
+      depositRequest,
+      signature
+    );
+
+    const receipt = await tx.wait(1);
+    const matchingEvents = parseEventsFromContractReceipt(
+      receipt,
+      this.depositManagerContract.interface.getEvent("DepositCompleted")
+    ) as DepositCompletedEvent[];
+    console.log("Matching events:", matchingEvents);
+
+    if (matchingEvents.length > 0) {
+      console.log(
+        `Deposit signed and submitted. Spender: ${depositRequest.spender}. Nonce: ${depositRequest.nonce}`
+      );
+      await this.db.setDepositRequestStatus(
+        depositRequest,
+        DepositRequestStatus.Completed
+      );
+    } else {
+      // NOTE: not sure if possible that tx submission passes but event not found
+      console.error(
+        `Deposit request failed. Spender: ${depositRequest.spender}. Nonce: ${depositRequest.nonce}`
+      );
     }
   }
 }
