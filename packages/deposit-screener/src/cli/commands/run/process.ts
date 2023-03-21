1eb7484a568ddcc57ec1647acdb97ef3dc275b8f
Luke Tchang <ltchang@stanford.edu>
2023-03-20 20:44:43 -0700

    fix(deposit-screener): move redis env stuff to cli, remove submitter

--- packages/deposit-screener/src/cli/commands/run/process.ts
+++ packages/deposit-screener/src/cli/commands/run/process.ts
@@ -1,13 +1,13 @@
 import { Command } from "commander";
 import { ethers } from "ethers";
 import { DepositScreenerProcessor } from "../../../processor";
-import { DepositScreenerSubmitter } from "../../../submitter";
 import { SubgraphScreenerSyncAdapter } from "../../../sync/subgraph/adapter";
+import { getRedis } from "../utils";
 
 const runProcess = new Command("process")
   .summary("Process deposit requests")
   .description(
-    "Must supply .env file with REDIS_URL, RPC_URL, TX_SIGNER_KEY, and SUBGRAPH_ENDPOINT. Must supply deposit manager contract address as options."
+    "Must supply .env file with REDIS_URL, RPC_URL, TX_SIGNER_KEY, and SUBGRAPH_URL. Must supply deposit manager contract address as options."
   )
   .requiredOption(
     "--deposit-manager-address <string>",
@@ -17,9 +17,9 @@ const runProcess = new Command("process")
     const { depositManagerAddress } = options;
 
     // TODO: enable switching on adapter impl
-    const subgraphEndpoint = process.env.SUGRAPH_ENDPOINT;
+    const subgraphEndpoint = process.env.SUBGRAPH_URL;
     if (!subgraphEndpoint) {
-      throw new Error("Missing SUBGRAPH_ENDPOINT");
+      throw new Error("Missing SUBGRAPH_URL");
     }
     const adapter = new SubgraphScreenerSyncAdapter(subgraphEndpoint);
 
@@ -27,16 +27,23 @@ const runProcess = new Command("process")
     if (!rpcUrl) {
       throw new Error("Missing RPC_URL");
     }
+
+    const privateKey = process.env.TX_SIGNER_KEY;
+    if (!privateKey) {
+      throw new Error("Missing TX_SIGNER_KEY");
+    }
+
     const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    const signingProvider = new ethers.Wallet(privateKey, provider);
 
     const processor = new DepositScreenerProcessor(
       adapter,
       depositManagerAddress,
-      provider
+      signingProvider,
+      getRedis()
     );
-    const submitter = new DepositScreenerSubmitter(depositManagerAddress);
 
-    await Promise.all([processor.run(), submitter.run()]);
+    await processor.run();
   });
 
 export default runProcess;
--- packages/deposit-screener/src/utils.ts
+++ packages/deposit-screener/src/utils.ts
@@ -1,18 +1,3 @@
-import IORedis from "ioredis";
-
 export function secsToMillis(seconds: number): number {
   return seconds * 1000;
-}
-
-export function getRedis(redis?: IORedis): IORedis {
-  if (redis) {
-    return redis;
-  } else {
-    const redisUrl = process.env.REDIS_URL ?? "localhost:6379";
-    const redisPassword = process.env.REDIS_PASSWORD;
-
-    return new IORedis(redisUrl, {
-      password: redisPassword,
-    });
-  }
 }
