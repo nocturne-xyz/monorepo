6c286bf2ef61958d5ab75147ff7afdb59b891a0a
Sladuca <sladuca777@gmail.com>
2023-03-31 19:59:35 -0400

    bubble up enqueue op failure

--- packages/bundler/src/router.ts
+++ packages/bundler/src/router.ts
@@ -74,7 +74,12 @@ export class BundlerRouter {
     }
 
     // Enqueue operation and add all inflight nullifiers
-    const jobId = await this.postJob(operation);
+    const jobId = await this.postJob(operation)
+      .catch(err => {
+        console.error("error posting job", err);
+        res.status(500).json({ error: "failed to enqueue operation" });
+      });
+
     res.json({ id: jobId });
   }
 
