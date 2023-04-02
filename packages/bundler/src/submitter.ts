cc0872fc022c8a2cf9a96521055d521c3ef22d9f
Sladuca <sladuca777@gmail.com>
2023-04-02 16:11:45 -0400

    remove nfs from bundler db when a bundle reverts

--- packages/bundler/src/submitter.ts
+++ packages/bundler/src/submitter.ts
@@ -11,7 +11,7 @@ import { Job, Worker } from "bullmq";
 import IORedis from "ioredis";
 import { ethers } from "ethers";
 import { OPERATION_BATCH_QUEUE, OperationBatchJobData } from "./common";
-import { StatusDB } from "./db";
+import { NullifierDB, StatusDB } from "./db";
 import * as JSON from "bigint-json-serialization";
 
 export interface BundlerSubmitterHandle {
@@ -26,6 +26,7 @@ export class BundlerSubmitter {
   signingProvider: ethers.Signer;
   walletContract: Wallet; // TODO: replace with tx manager
   statusDB: StatusDB;
+  nullifierDB: NullifierDB;
 
   readonly INTERVAL_SECONDS: number = 60;
   readonly BATCH_SIZE: number = 8;
@@ -37,6 +38,7 @@ export class BundlerSubmitter {
   ) {
     this.redis = redis;
     this.statusDB = new StatusDB(this.redis);
+    this.nullifierDB = new NullifierDB(this.redis);
     this.signingProvider = signingProvider;
     this.walletContract = Wallet__factory.connect(
       walletAddress,
@@ -109,21 +111,25 @@ export class BundlerSubmitter {
       );
     } catch (err) {
       console.log("failed to process bundle:", err);
-      const statusTxs = operations.map((op) => {
+      const redisTxs = operations.flatMap((op) => {
         const digest = computeOperationDigest(op);
         console.log(
           `setting operation with digest ${digest} to BUNDLE_REVERTED`
         );
-        return this.statusDB.getSetJobStatusTransaction(
+        const statusTx = this.statusDB.getSetJobStatusTransaction(
           digest.toString(),
           OperationStatus.BUNDLE_REVERTED
         );
+
+        const nullifierTx = this.nullifierDB.getRemoveNullifierTransactions(op);
+
+        return [statusTx, nullifierTx];
       });
 
-      await this.redis.multi(statusTxs).exec((maybeErr) => {
+      await this.redis.multi(redisTxs).exec((maybeErr) => {
         if (maybeErr) {
           throw new Error(
-            `failed to set job status transactions after bundle reverted: ${maybeErr}`
+            `failed to set job statuses and/or remove nullifiers after bundle reverted: ${maybeErr}`
           );
         }
       });
