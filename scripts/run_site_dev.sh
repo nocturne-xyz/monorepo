a4240344eb52158556bc4e92a52bc4dab558ccfb
Sladuca <sladuca777@gmail.com>
2023-04-10 12:43:05 -0400

    make site script logs lowercase

--- scripts/run_site_dev.sh
+++ scripts/run_site_dev.sh
@@ -121,7 +121,7 @@ popd
 docker compose -f ./packages/bundler/docker-compose.yml --env-file packages/bundler/.env up --build &> "$LOG_DIR/bundler-docker-compose" &
 BUNDLER_PID=$!
 
-echo "Bundler running at PID: $BUNDLER_PID"
+echo "bundler running at PID: $BUNDLER_PID"
 
 # write screener's .env file
 pushd packages/deposit-screener
@@ -146,7 +146,7 @@ popd
 docker compose -f ./packages/deposit-screener/docker-compose.yml --env-file packages/deposit-screener/.env up --build  &> "$LOG_DIR/screener-docker-compose" &
 SCREENER_PID=$!
 
-echo "Screener running at PID: $SCREENER_PID"
+echo "screener running at PID: $SCREENER_PID"
 
 # write subtree updater's .env file
 pushd packages/subtree-updater
@@ -160,7 +160,7 @@ popd
 docker run --env-file ./packages/subtree-updater/.env --add-host host.docker.internal:host-gateway docker.io/library/mock-subtree-updater --use-mock-prover --fill-batches --handler-address "$HANDLER_CONTRACT_ADDRESS" --zkey-path ./circuit-artifacts/subtreeupdate/subtreeupdate_cpp/subtreeupdate.zkey --vkey-path ./circuit-artifacts/subtreeupdate/subtreeupdate_cpp/vkey.json --prover-path /rapidsnark/build/prover --witness-generator-path ./circuit-artifacts/subtreeupdate/subtreeupdate_cpp/subtreeupdate &> "$LOG_DIR/subtree-updater" &
 SUBTREE_UPDATER_PID=$!
 
-echo "Subtree updater running at PID: $SUBTREE_UPDATER_PID"
+echo "subtree updater running at PID: $SUBTREE_UPDATER_PID"
 
 SNAP_INDEX_TS="$SCRIPT_DIR/../packages/snap/src/index.ts"
 SITE_TEST_PAGE="$SCRIPT_DIR/../packages/site/src/pages/index.tsx"
