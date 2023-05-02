29f28d6d1088ced3b319dc543881043d7199de81
Luke Tchang <ltchang@stanford.edu>
2023-05-02 14:39:15 -0700

    fix(config): switch statement builtin

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -108,13 +108,14 @@ export function loadNocturneConfig(
 }
 
 export function loadNocturneConfigBuiltin(name: string): NocturneConfig {
-  if (name == "sepolia") {
-    return NocturneConfig.fromObject(sepolia as any);
-  } else if (name == "localhost") {
-    return NocturneConfig.fromObject(localhost as any);
-  } else if (name == "example-network") {
-    return NocturneConfig.fromObject(exampleNetwork as any);
-  } else {
-    throw new Error(`unknown config name: ${name}`);
+  switch (name) {
+    case "sepolia":
+      return NocturneConfig.fromObject(sepolia as any);
+    case "localhost":
+      return NocturneConfig.fromObject(localhost as any);
+    case "example-network":
+      return NocturneConfig.fromObject(exampleNetwork as any);
+    default:
+      throw new Error(`unknown config name: ${name}`);
   }
 }
