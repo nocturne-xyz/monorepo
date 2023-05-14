f26b7556cda650898cdc999950ace758a7566d4a
Luke Tchang <ltchang@stanford.edu>
2023-05-14 10:55:55 -0700

    docs(contracts): reentrancy guard docs + changelog (#248)

--- packages/contracts/contracts/NocturneReentrancyGuard.sol
+++ packages/contracts/contracts/NocturneReentrancyGuard.sol
@@ -3,21 +3,32 @@ pragma solidity ^0.8.17;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-// Modified from ReentrancyGuard.sol from OpenZeppelin contracts
+/// @title NocturneReentrancyGuard
+/// @author Nocturne Labs
+/// @notice Custom reentrancy guard that track stages of operation processing/execution.
+/// @dev Modified from OpenZeppelin ReentrancyGuard.sol
 contract NocturneReentrancyGuard is Initializable {
+    // No operation entered (before Teller calls handler.handleOperation)
     uint256 public constant NOT_ENTERED = 1;
+    // Once an operation is entered for processing (after Teller calls handler.handleOperation)
     uint256 public constant ENTERED_HANDLE_OPERATION = 2;
+    // Once an operation is entered for execution (after Teller calls handler.executeActions)
     uint256 public constant ENTERED_EXECUTE_ACTIONS = 3;
 
+    // Operation stage
     uint256 private _operationStage;
 
-    // gap for upgrade safety
+    // Gap for upgrade safety
     uint256[50] private __GAP;
 
+    /// @notice Internal initializer
     function __NocturneReentrancyGuard_init() internal onlyInitializing {
         _operationStage = NOT_ENTERED;
     }
 
+    /// @notice Requires current stage to be NOT_ENTERED.
+    /// @dev Moves stage to ENTERED_HANDLE_OPERATION before function execution, then resets stage
+    ///      to NOT_ENTERED at end of call context.
     modifier handleOperationGuard() {
         require(_operationStage == NOT_ENTERED, "Reentry into handleOperation");
         _operationStage = ENTERED_HANDLE_OPERATION;
@@ -27,6 +38,9 @@ contract NocturneReentrancyGuard is Initializable {
         _operationStage = NOT_ENTERED;
     }
 
+    /// @notice Requires current stage to be ENTERED_HANDLE_OPERATION.
+    /// @dev Moves stage to ENTERED_EXECUTE_ACTIONS before function execution, then resets stage to
+    ///      ENTERED_HANDLE_OPERATION at end of call context.
     modifier executeActionsGuard() {
         require(
             _operationStage == ENTERED_HANDLE_OPERATION,
@@ -39,10 +53,7 @@ contract NocturneReentrancyGuard is Initializable {
         _operationStage = ENTERED_HANDLE_OPERATION;
     }
 
-    /**
-     * @dev Returns true if the reentrancy guard is currently set to "entered", which indicates there is a
-     * `nonReentrant` function in the call stack.
-     */
+    /// @notice Returns current operation stage
     function reentrancyGuardStage() public view returns (uint256) {
         return _operationStage;
     }
