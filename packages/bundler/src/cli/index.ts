e48cbb9c848058dd01339fa0781cdc484485e589
Sladuca <sladuca777@gmail.com>
2023-05-21 17:20:17 -0400

    hack: manually invoke process.exit() when main returns

--- packages/bundler/src/cli/index.ts
+++ packages/bundler/src/cli/index.ts
@@ -14,4 +14,16 @@ export default async function main(): Promise<void> {
   await program.parseAsync(process.argv);
 }
 
-main().catch((e) => console.log(`bundler exited with error: ${e}`));
+// ! HACK
+// ! in principle, there should be no hanging promises once `main()` resolves or rejects.
+// ! as a backstop, we manually call `process.exit()` to ensure the process actually exits
+// ! even if there's a bug somewhere that results in a hanging promise
+main()
+  .then(() => {
+    console.log(`deposit-screener-cli ran to completion`);
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.log(`bundler-cli exited with error: ${e}`);
+    process.exit(1);
+  });
