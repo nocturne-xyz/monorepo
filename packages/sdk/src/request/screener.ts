1a27c19fe3c9fd74895b09b2adbaa2bd2af6b484
Luke Tchang <ltchang@stanford.edu>
2023-05-22 18:20:30 -0400

    save sdk pre publish

---
+++ packages/sdk/src/request/screener.ts
@@ -0,0 +1,16 @@
+import { Address, DepositRequestStatus } from "../primitives";
+
+export interface DepositQuoteRequest {
+  spender: Address;
+  assetAddr: Address;
+  value: bigint;
+}
+
+export interface DepositQuoteResponse {
+  estimatedWaitSeconds: number;
+}
+
+export interface DepositStatusResponse {
+  status: DepositRequestStatus;
+  estimatedWaitSeconds: number;
+}
