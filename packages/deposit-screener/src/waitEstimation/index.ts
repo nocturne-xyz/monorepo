864e3158451e3df73fd880f3b38b1f7e9e61ae0f
Luke Tchang <ltchang@stanford.edu>
2023-05-21 22:45:07 -0400

    refactor(screener): clean up time/queue separation + easier scenario based logic

--- packages/deposit-screener/src/waitEstimation/index.ts
+++ packages/deposit-screener/src/waitEstimation/index.ts
@@ -1,12 +1,7 @@
 import { Job, Queue } from "bullmq";
 import { DepositScreenerDB } from "../db";
 import { DepositRequestJobData } from "../types";
-import {
-  Address,
-  AssetTrait,
-  DepositRequest,
-  DepositRequestStatus,
-} from "@nocturne-xyz/sdk";
+import { Address, AssetTrait, DepositRequestStatus } from "@nocturne-xyz/sdk";
 import { ScreenerDelayCalculator } from "../screenerDelay";
 import { ScreeningApi } from "../screening";
 import {
