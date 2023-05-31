ffe58fa757997e0e927df973871234afb98848a2
Sladuca <sladuca777@gmail.com>
2023-05-31 16:50:42 -0400

    add point (de)compression to sdk

--- packages/sdk/src/crypto/address.ts
+++ packages/sdk/src/crypto/address.ts
@@ -6,13 +6,22 @@ import {
 import { assertOrErr } from "../utils";
 import * as JSON from "bigint-json-serialization";
 import { randomFr } from "./utils";
-import { decodePoint } from "./noteEncryption";
+import {
+  CompressedPoint,
+  compressPoint,
+  decompressPoint,
+} from "./pointEncoding";
 
 export interface StealthAddress {
   h1X: bigint;
   h1Y: bigint;
   h2X: bigint;
   h2Y: bigint;
+}
+
+export interface CompressedStealthAddress {
+  h1: CompressedPoint;
+  h2: CompressedPoint;
 }
 
 export type CanonAddress = AffinePoint<bigint>;
@@ -40,11 +49,34 @@ export class StealthAddressTrait {
     };
   }
 
-  static fromCompressedPoints(H1X: bigint, H2X: bigint): StealthAddress {
-    const H1 = decodePoint(H1X);
-    const H2 = decodePoint(H2X);
+  // TODO: remove
+  static fromCompressedPoints(
+    H1Comprsesed: CompressedPoint,
+    H2Compressed: CompressedPoint
+  ): StealthAddress {
+    const H1 = decompressPoint(H1Comprsesed);
+    const H2 = decompressPoint(H2Compressed);
+
+    if (!H1 || !H2) {
+      throw new Error("Invalid stealth address");
+    }
 
     return StealthAddressTrait.fromPoints({ h1: H1, h2: H2 });
+  }
+
+  static decompress(address: CompressedStealthAddress): StealthAddress {
+    return StealthAddressTrait.fromCompressedPoints(address.h1, address.h2);
+  }
+
+  static compress(address: StealthAddress): CompressedStealthAddress {
+    const points = StealthAddressTrait.toPoints(address);
+    const h1 = compressPoint(points.h1);
+    const h2 = compressPoint(points.h2);
+
+    return {
+      h1,
+      h2,
+    };
   }
 
   static toString(address: StealthAddress): string {
