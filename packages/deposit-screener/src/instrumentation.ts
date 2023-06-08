f7ea23b5284d4a6b1d34393ad42d9e69ad808f2a
Luke Tchang <ltchang@stanford.edu>
2023-06-07 22:38:51 -0700

    fix(screener): pipes to dd agent

--- packages/deposit-screener/src/instrumentation.ts
+++ packages/deposit-screener/src/instrumentation.ts
@@ -1,11 +1,11 @@
 import opentelemetry from "@opentelemetry/api";
 import {
   MeterProvider,
-  // PeriodicExportingMetricReader,
+  PeriodicExportingMetricReader,
 } from "@opentelemetry/sdk-metrics";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 
 const resource = Resource.default().merge(
   new Resource({
@@ -14,25 +14,25 @@ const resource = Resource.default().merge(
   })
 );
 
-// const collectorOptions = {
-//   url: undefined, // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-//   headers: {}, // an optional object containing custom headers to be sent with each request
-//   concurrencyLimit: 1, // an optional limit on pending requests
-// };
-const metricExporter = new PrometheusExporter();
+const collectorOptions = {
+  url: "http://datadog-agent:4318/v1/metrics", // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+  headers: {}, // an optional object containing custom headers to be sent with each request
+  concurrencyLimit: 1, // an optional limit on pending requests
+};
+const metricExporter = new OTLPMetricExporter(collectorOptions);
 
-// const metricReader = new PeriodicExportingMetricReader({
-//   exporter: metricExporter,
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricExporter,
 
-//   // Default is 60000ms (60 seconds). Set to 3 seconds for demonstrative purposes only.
-//   exportIntervalMillis: 3000,
-// });
+  // Default is 60000ms (60 seconds). Set to 3 seconds for demonstrative purposes only.
+  exportIntervalMillis: 3000,
+});
 
 const myServiceMeterProvider = new MeterProvider({
   resource: resource,
 });
 
-myServiceMeterProvider.addMetricReader(metricExporter);
+myServiceMeterProvider.addMetricReader(metricReader);
 
 // Set this MeterProvider to be global to the app being instrumented.
 opentelemetry.metrics.setGlobalMeterProvider(myServiceMeterProvider);
