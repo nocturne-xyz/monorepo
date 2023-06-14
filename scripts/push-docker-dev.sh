4f8a77bd1554787e1bec3295bec324638979c1bb
Ubuntu <ubuntu@ip-172-31-21-23.ec2.internal>
2023-06-14 15:21:49 +0000

    fix docker push script

--- scripts/push-docker-dev.sh
+++ scripts/push-docker-dev.sh
@@ -1,5 +1,9 @@
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
+docker tag nocturnelabs/bundler:dev "nocturnelabs/bundler:$COMMIT_HASH"
+docker tag nocturnelabs/deposit-screener:dev "nocturnelabs/deposit-screener:$COMMIT_HASH"
+docker tag nocturnelabs/subtree-updater:dev "nocturnelabs/subtree-updater:$COMMIT_HASH"
+
 docker push "nocturnelabs/bundler:$COMMIT_HASH"
 docker push "nocturnelabs/deposit-screener:$COMMIT_HASH"
 docker push "nocturnelabs/subtree-updater:$COMMIT_HASH"
