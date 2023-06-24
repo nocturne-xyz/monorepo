c72bff6beb966bd1ce86a6d6e8993cca94acc0c6
Luke Tchang <ltchang@stanford.edu>
2023-06-24 12:22:03 -0700

    fix: +4s to subgraph wait

--- packages/e2e-tests/src/deploy.ts
+++ packages/e2e-tests/src/deploy.ts
@@ -209,7 +209,7 @@ export async function setupTestDeployment(
     actorConfig.configs.subgraph = subgraphConfig;
 
     stopSubgraph = await startSubgraph(subgraphConfig);
-    await sleep(8_000); // wait for subgraph to start up (TODO: better way to do this?)
+    await sleep(12_000); // wait for subgraph to start up (TODO: better way to do this?)
   }
 
   // deploy everything else
