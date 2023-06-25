21a5bac223fe7d0d0d12fef2f33afc71821113e8
Luke Tchang <ltchang@stanford.edu>
2023-06-25 13:33:10 -0700

    prog(test-actor): lmdb impl

---
+++ packages/test-actor/src/lmdb.ts
@@ -0,0 +1,116 @@
+import { KV, KVStore, zip } from "@nocturne-xyz/sdk";
+import lmdb, { open, Database } from "lmdb";
+
+interface LMDBKvStoreOpts {
+  path?: string;
+}
+
+export class LMDBKVStore implements KVStore {
+  private db: Database;
+
+  constructor(opts?: LMDBKvStoreOpts) {
+    this.db = open({ path: opts?.path ?? "./db" });
+  }
+
+  async getString(key: string): Promise<string | undefined> {
+    return this.db.get(key);
+  }
+
+  async putString(key: string, value: string): Promise<boolean> {
+    return this.db.put(key, value);
+  }
+
+  async remove(key: string): Promise<boolean> {
+    return this.db.remove(key);
+  }
+
+  async containsKey(key: string): Promise<boolean> {
+    return this.db.get(key) !== undefined;
+  }
+
+  async getNumber(key: string): Promise<number | undefined> {
+    const value = await this.db.get(key);
+    return value ? Number(value) : undefined;
+  }
+
+  async putNumber(key: string, value: number): Promise<boolean> {
+    return this.db.put(key, value);
+  }
+
+  async getBigInt(key: string): Promise<bigint | undefined> {
+    const value = await this.db.get(key);
+    return value ? BigInt(value) : undefined;
+  }
+
+  async putBigInt(key: string, value: bigint): Promise<boolean> {
+    return this.db.put(key, value);
+  }
+
+  async iterRange(
+    startKey: string,
+    endKey: string
+  ): Promise<AsyncIterable<KV>> {
+    const lmdbIter = this.db.getRange({
+      start: startKey,
+      end: endKey,
+      versions: false,
+    });
+    return toAsyncIterableKV(lmdbIter);
+  }
+
+  async iterPrefix(prefix: string): Promise<AsyncIterable<KV>> {
+    const iter = this.db
+      .getRange({ versions: false })
+      .filter((kv) => kv.key.toString().startsWith(prefix));
+    return toAsyncIterableKV(iter);
+  }
+
+  async putMany(kvs: KV[]): Promise<boolean> {
+    const tx = this.db.transaction(() => {
+      for (const [key, value] of kvs) {
+        this.db.put(key, value);
+      }
+    });
+    await tx;
+    return true;
+  }
+
+  async getMany(keys: string[]): Promise<KV[]> {
+    const values = (await this.db.getMany(keys)).map((v) => String(v));
+    return zip(keys, values);
+  }
+
+  async removeMany(keys: string[]): Promise<boolean> {
+    const tx = this.db.transaction(() => {
+      for (const key of keys) {
+        this.db.remove(key);
+      }
+    });
+    await tx;
+    return true;
+  }
+
+  async clear(): Promise<void> {
+    await this.db.clearAsync();
+  }
+
+  async close(): Promise<void> {
+    return;
+  }
+}
+
+function toAsyncIterableKV(
+  iterable: lmdb.RangeIterable<{
+    key: lmdb.Key;
+    value: any;
+    version?: number | undefined;
+  }>
+): AsyncIterable<KV> {
+  return {
+    [Symbol.asyncIterator]: async function* () {
+      for (const { key, value } of iterable) {
+        yield [String(key), String(value)];
+      }
+    },
+  };
+}
