ce8e72912f3c7bd6b817922d856bbab10c214f59
Daniel Park <danielpark1140@gmail.com>
2023-07-27 18:33:35 -0700

    Screener monkey patches (#374)
    * Monkey patches
    
    * screener src

--- packages/deposit-screener/src/screening/dummy.ts
+++ packages/deposit-screener/src/screening/dummy.ts
@@ -1,9 +1,8 @@
 import { Address } from "@nocturne-xyz/sdk";
 import { ScreeningApi } from ".";
+import { dummySafeDepositCheck } from "../utils";
 
 export class DummyScreeningApi implements ScreeningApi {
-  magicRejectionValue = 30303000000000000n; // 0.030303 for 18 decimals
-
   async isSafeDepositRequest(
     spender: Address,
     assetAddr: Address,
@@ -11,11 +10,6 @@ export class DummyScreeningApi implements ScreeningApi {
   ): Promise<boolean> {
     spender;
     assetAddr;
-
-    if (this.magicRejectionValue && value === this.magicRejectionValue) {
-      return false;
-    }
-
-    return true;
+    return dummySafeDepositCheck(value);
   }
 }
