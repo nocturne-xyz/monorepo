7b604072523331c5b6ec81a45dc0efa2988ef919
Luke Tchang <ltchang@stanford.edu>
2023-08-04 15:44:02 -0400

    fix: comment nits in validation

--- packages/contracts/contracts/libs/Validation.sol
+++ packages/contracts/contracts/libs/Validation.sol
@@ -17,10 +17,10 @@ library Validation {
     function validateOperation(Operation calldata op) internal view {
         // Ensure public spend > 0 for public joinsplit. Ensures handler only deals
         // with assets that are actually unwrappable. If asset has > 0 public spend, then
-        // circuit guarantees that the _revealed_ asset is included in the tree and
-        // unwrappable. If asset has public spend = 0, circuit guarantees that the _masked_
-        // asset is included in the tree and unwrappable, but the revealed asset for public
-        // spend = 0 is (0,0) and not unwrappable.
+        // circuit guarantees that note with the _revealed_ asset is included in the tree is
+        // unwrappable. If asset has public spend = 0, circuit guarantees that the note with the
+        // _masked_ asset is included in the tree and unwrappable, but the revealed asset for public
+        // spend = 0 is (0,0) and is not unwrappable.
         for (uint256 i = 0; i < op.pubJoinSplits.length; i++) {
             require(op.pubJoinSplits[i].publicSpend > 0, "0 public spend");
         }
