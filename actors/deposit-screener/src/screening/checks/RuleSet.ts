eb34c76c5be00e29c0c968179e8c06c072071ff2
danielpark <danielpark1140@gmail.com>
2023-08-30 15:56:34 -0700

    poc complete

---
+++ actors/deposit-screener/src/screening/checks/RuleSet.ts
@@ -0,0 +1,69 @@
+import { DepositRequest } from "@nocturne-xyz/core";
+import { API_CALLS, Data } from "./apiCalls";
+
+export type Rejection = { type: "Rejection"; reason: string };
+export type Delay = { type: "Delay"; timeSeconds: number };
+
+export class Rule<T extends Data> {
+  public next: Rule<any> | null;
+  private threshold: (data: T) => boolean;
+  private call: keyof typeof API_CALLS;
+  private action: Rejection | Delay;
+  constructor({
+    call,
+    threshold,
+    action,
+  }: {
+    call: keyof typeof API_CALLS;
+    threshold: (data: T) => boolean;
+    action: Rejection | Delay;
+  }) {
+    this.next = null;
+    this.call = call;
+    this.threshold = threshold;
+    this.action = action;
+  }
+
+  async check(
+    deposit: DepositRequest,
+    cache: Record<string, Data>
+  ): Promise<Rejection | Delay | undefined> {
+    if (!cache[this.call]) {
+      cache[this.call] = await API_CALLS[this.call](deposit);
+    }
+    const data = cache[this.call] as T;
+    return this.threshold(data) ? this.action : undefined;
+  }
+}
+
+export class RuleSet {
+  private head: Rule<any> | null = null;
+  private tail: Rule<any> | null = null;
+  private delay = 0;
+
+  add<T extends Data>(rule: Rule<T>): RuleSet {
+    if (!this.head) {
+      this.head = rule;
+    } else {
+      this.tail!.next = rule;
+    }
+    this.tail = rule;
+    return this;
+  }
+
+  async check(deposit: DepositRequest): Promise<Rejection | Delay> {
+    let currRule = this.head;
+    const cache: Record<string, Data> = {};
+    while (currRule !== null) {
+      const result = await currRule.check(deposit, cache);
+      if (result) {
+        if (result.type === "Rejection") {
+          return result;
+        }
+        this.delay += result.timeSeconds;
+      }
+      currRule = currRule.next;
+    }
+    return { type: "Delay", timeSeconds: this.delay };
+  }
+}
---
+++ actors/deposit-screener/src/screening/checks/apiCalls.ts
@@ -0,0 +1,23 @@
+import { DepositRequest } from "@nocturne-xyz/core";
+
+export type TrmData = {
+  risk: number; // TODO, use vals from response
+};
+export type MisttrackData = {
+  misttrackRisk: number;
+};
+
+export type Data = TrmData | MisttrackData;
+
+export const API_CALLS = {
+  // {{TRM_URL}}/public/v2/screening/addresses
+  TRM_SCREENING_ADDRESSES: async (deposit: DepositRequest) => {
+    console.log(deposit);
+    return await Promise.resolve({ risk: 0.5 });
+  },
+  // {{MISTTRACK_BASE_URL}}/risk_score
+  MISTTRACK_ADDRESS_RISK_SCORE: async (deposit: DepositRequest) => {
+    console.log(deposit);
+    return await Promise.resolve({ misttrackRisk: 0.5 });
+  },
+} as const;
