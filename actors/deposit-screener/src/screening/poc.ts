050a555cfdc2cc47ee589cc71bef2142765ea752
danielpark <danielpark1140@gmail.com>
2023-08-30 11:37:35 -0700

    compiles

--- actors/deposit-screener/src/screening/poc.ts
+++ actors/deposit-screener/src/screening/poc.ts
@@ -12,18 +12,18 @@ type MisttrackData = {
 
 type Data = TrmData | MisttrackData;
 
-class Rule {
-  next: Rule | null;
-  threshold: (data: Data) => boolean;
-  call: Thunk<Data, [DepositRequest]>;
+class Rule<T extends Data> {
+  next: Rule<any> | null;
+  threshold: (data: T) => boolean;
+  call: Thunk<T, [DepositRequest]>;
   action: Rejection | Delay;
   constructor({
     call,
     threshold,
     action,
   }: {
-    call: (deposit: DepositRequest) => Promise<Data>;
-    threshold: (data: Data) => boolean;
+    call: (deposit: DepositRequest) => Promise<T>;
+    threshold: (data: T) => boolean;
     action: Rejection | Delay;
   }) {
     this.next = null;
@@ -39,13 +39,13 @@ class Rule {
 }
 
 class RuleSet {
-  head: Rule | null = null;
-  tail: Rule | null = null;
+  head: Rule<any> | null = null;
+  tail: Rule<any> | null = null;
   delay = 0;
 
   constructor() {}
 
-  add(rule: Rule) {
+  add<T extends Data>(rule: Rule<T>) {
     if (!this.head) {
       this.head = rule;
     } else {
@@ -77,12 +77,12 @@ class RuleSet {
 // todo make facade for both TRM & Misttrack APIs
 // todo reuse calls to apis, by extracting it out and making properties it returns usable by actionthreshold
 const TRM_RULE_1 = new Rule({
-  call: () => Promise.resolve({ risk: 0.5 }),
+  // > $0 of ownership exposure to severe risk categories
+  call: (deposit: DepositRequest) => Promise.resolve({ risk: 0.5 }),
   threshold(data: TrmData) {
     return data.risk > 0.5; // TODO actually implement
   },
   action: { type: "Rejection", reason: "Risk is too high" },
 });
-// > $0 of ownership exposure to severe risk categories
 
 const RULESET_V1 = new RuleSet().add(TRM_RULE_1);
