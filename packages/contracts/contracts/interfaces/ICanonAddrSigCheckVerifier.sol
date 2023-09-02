a15c3e12d7d7c8955ee2c53fbc39b7fb4b272fb8
Sladuca <sladuca777@gmail.com>
2023-09-02 12:17:53 -0400

    build circuit

---
+++ packages/contracts/contracts/interfaces/ICanonAddrSigCheckVerifier.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import {IVerifier} from "./IVerifier.sol";
+
+/// @title Verifier interface.
+/// @dev Interface of Verifier contract.
+interface ISubtreeUpdateVerifier is IVerifier {
+
+}
