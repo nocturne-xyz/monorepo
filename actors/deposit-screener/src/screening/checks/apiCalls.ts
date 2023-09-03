268075daf5eed9b0e82b561309b4d0a7d1db323b
danielpark <danielpark1140@gmail.com>
2023-09-03 13:49:27 -0700

    misttrack call with test

--- actors/deposit-screener/src/screening/checks/apiCalls.ts
+++ actors/deposit-screener/src/screening/checks/apiCalls.ts
@@ -1,6 +1,6 @@
 import { ScreeningDepositRequest } from "..";
 
-export interface AddressRiskIndicator {
+export interface TrmAddressRiskIndicator {
   category: string;
   categoryId: string;
   categoryRiskScoreLevel: number;
@@ -15,19 +15,49 @@ export interface TrmData {
   addressIncomingVolumeUsd: string;
   addressOutgoingVolumeUsd: string;
   addressTotalVolumeUsd: string;
-  addressRiskIndicators: AddressRiskIndicator[];
+  addressRiskIndicators: TrmAddressRiskIndicator[];
 }
 
-export interface MisttrackData {
-  balance: number;
-  txs_count: number;
-  first_seen: number;
-  last_seen: number;
-  total_received: number;
-  total_spent: number;
-  received_txs_count: number;
-  spent_txs_count: number;
+export interface MisttrackRiskDetail {
+  label: string;
+  type: string;
+  volume: number;
+  address: string;
+  percent: number;
 }
+
+export type MisttrackRiskItem =
+  | "Malicious Address"
+  | "Suspected Malicious Address"
+  | "High-risk Tag Address"
+  | "Medium-risk Tag Address"
+  | "Mixer"
+  | "Risk Exchange"
+  | "Gambling"
+  | "Involved Theft Activity"
+  | "Involved Ransom Activity"
+  | "Involved Phishing Activity"
+  | "Interact With Malicious Address"
+  | "Interact With Suspected Malicious Address"
+  | "Interact With High-risk Tag Address"
+  | "Interact With Medium-risk Tag Addresses";
+
+export interface MisttrackData {
+  score: number;
+  hacking_event: string;
+  detail_list: MisttrackRiskItem[];
+  risk_level: "Low" | "Moderate" | "High" | "Severe";
+  risk_detail: MisttrackRiskDetail[];
+}
+export type MisttrackApiResponse =
+  | {
+      success: false;
+      msg: string;
+    }
+  | {
+      success: true;
+      data: MisttrackData;
+    };
 
 export interface DummyTrmData {
   risk: number;
@@ -36,7 +66,7 @@ export interface DummyMisttrackData {
   misttrackRisk: number;
 }
 
-export type Data = DummyTrmData | DummyMisttrackData;
+export type Data = TrmData | MisttrackData | DummyTrmData | DummyMisttrackData;
 
 const TRM_BASE_URL = "https://api.trmlabs.com/public/v2";
 const MISTTRACK_BASE_URL = "https://openapi.misttrack.io/v1";
@@ -70,11 +100,16 @@ export const API_CALLS = {
     token = "ETH"
   ): Promise<MisttrackData> => {
     const response = await fetch(
-      `${MISTTRACK_BASE_URL}/address_overview?coin=${token}&address=${deposit.spender}&api_key=${MISTTRACK_API_KEY}`
+      `${MISTTRACK_BASE_URL}/risk_score?coin=${token}&address=${deposit.spender}&api_key=${MISTTRACK_API_KEY}`
     );
-    const data = (await response.json()) as MisttrackData;
-    console.log(data);
-    return data;
+    const misttrackResponse = (await response.json()) as MisttrackApiResponse;
+    if (!misttrackResponse.success) {
+      throw new Error(
+        `Call to misttrack failed with message: ${misttrackResponse.msg}`
+      );
+    }
+    console.log(misttrackResponse.data);
+    return misttrackResponse.data;
   },
 
   DUMMY_TRM_SCREENING_ADDRESSES: async (deposit: ScreeningDepositRequest) => {
