6042cf89a2b078c349a4c3bf8c3c3904d7f6c8bb
Luke Tchang <ltchang@stanford.edu>
2023-09-05 20:55:36 -0400

    add canonical address registry contract

--- packages/contracts/contracts/libs/Utils.sol
+++ packages/contracts/contracts/libs/Utils.sol
@@ -10,6 +10,18 @@ library Utils {
     uint256 public constant BN254_SCALAR_FIELD_MODULUS =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
+    uint256 constant COMPRESSED_POINT_SIGN_MASK = 1 << 254;
+
+    // takes a compressed point and extracts the sign bit and y coordinate
+    // returns (sign, y)
+    function decomposeCompressedPoint(
+        uint256 compressedPoint
+    ) internal pure returns (uint256 sign, uint256 y) {
+        sign = (compressedPoint & COMPRESSED_POINT_SIGN_MASK) >> 254;
+        y = compressedPoint & (COMPRESSED_POINT_SIGN_MASK - 1);
+        return (sign, y);
+    }
+
     // return the minimum of the two values
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return (a >= b) ? b : a;
