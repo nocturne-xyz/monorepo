41a2033813d7520d054a3687cd46d94ff6f5625b
Luke Tchang <ltchang@stanford.edu>
2023-09-10 14:06:18 -0400

    try/catch in uniswap plugin, turn off async executor lint to warn

--- packages/op-request-plugins/src/UniswapV3Plugin.ts
+++ packages/op-request-plugins/src/UniswapV3Plugin.ts
@@ -70,112 +70,114 @@ export function UniswapV3Plugin<EInner extends BaseOpRequestBuilder>(
       tokenOut: Address,
       maxSlippageBps?: number
     ) {
-      const prom = new Promise<BuilderItemToProcess>(async (resolve) => {
-        const router = this.getSwapRouter();
-        const handlerAddress = this.config.handlerAddress();
+      const prom = new Promise<BuilderItemToProcess>(
+        async (resolve, reject) => {
+          try {
+            const router = this.getSwapRouter();
+            const handlerAddress = this.config.handlerAddress();
 
-        const erc20InContract = new ethers.Contract(tokenIn, ERC20_ABI);
-        const tokenInDecimals: number = Number(
-          await erc20InContract.decimals()
-        );
-        const tokenInSymbol: string = await erc20InContract.symbol();
-        const tokenInName: string = await erc20InContract.name();
+            const erc20InContract = new ethers.Contract(tokenIn, ERC20_ABI);
+            const tokenInDecimals = Number(await erc20InContract.decimals());
+            const tokenInSymbol: string = await erc20InContract.symbol();
+            const tokenInName: string = await erc20InContract.name();
 
-        const erc20OutContract = new ethers.Contract(tokenOut, ERC20_ABI);
-        const tokenOutDecimals: number = Number(
-          await erc20OutContract.decimals()
-        );
-        const tokenOutSymbol: string = await erc20OutContract.symbol();
-        const tokenOutName: string = await erc20OutContract.name();
+            const erc20OutContract = new ethers.Contract(tokenOut, ERC20_ABI);
+            const tokenOutDecimals = Number(await erc20OutContract.decimals());
+            const tokenOutSymbol: string = await erc20OutContract.symbol();
+            const tokenOutName: string = await erc20OutContract.name();
 
-        const swapOpts: SwapOptionsUniversalRouter = {
-          type: SwapType.UNIVERSAL_ROUTER,
-          simulate: {
-            fromAddress: handlerAddress,
-          },
-          slippageTolerance: new Percent(maxSlippageBps ?? 50, 10_000),
-          recipient: handlerAddress,
-          deadlineOrPreviousBlockhash: Date.now() + 3_600,
-        };
-        const chainId = chainIdToUniswapChainIdType(this._op.chainId);
-        const route = await router.route(
-          CurrencyAmount.fromRawAmount(
-            new Token(
-              chainId,
+            const swapOpts: SwapOptionsUniversalRouter = {
+              type: SwapType.UNIVERSAL_ROUTER,
+              simulate: {
+                fromAddress: handlerAddress,
+              },
+              slippageTolerance: new Percent(maxSlippageBps ?? 50, 10_000),
+              recipient: handlerAddress,
+              deadlineOrPreviousBlockhash: Date.now() + 3_600,
+            };
+            const chainId = chainIdToUniswapChainIdType(this._op.chainId);
+            const route = await router.route(
+              CurrencyAmount.fromRawAmount(
+                new Token(
+                  chainId,
+                  tokenIn,
+                  tokenInDecimals,
+                  tokenInSymbol,
+                  tokenInName
+                ),
+                Number(inAmount) // TODO: truncation ok?
+              ),
+              new Token(
+                chainId,
+                tokenOut,
+                tokenOutDecimals,
+                tokenOutSymbol,
+                tokenOutName
+              ),
+              TradeType.EXACT_INPUT,
+              swapOpts
+            );
+
+            if (!route) {
+              throw new Error(
+                `No route found for swap. Token in: ${tokenIn}, Token out: ${tokenOut}. Amount in: ${inAmount}`
+              );
+            }
+
+            const unwrap: UnwrapRequest = {
+              asset: AssetTrait.erc20AddressToAsset(tokenIn),
+              unwrapValue: inAmount,
+            };
+
+            const swapAction: Action = {
+              contractAddress: UNIVERSAL_SWAP_ROUTER_ADDRESS,
+              encodedFunction: route.methodParameters!.calldata,
+            };
+
+            const refundAsset = AssetTrait.erc20AddressToAsset(tokenOut);
+
+            const metadata: OperationMetadataItem = {
+              type: "Action",
+              actionType: "UniswapV3 Swap",
               tokenIn,
-              tokenInDecimals,
-              tokenInSymbol,
-              tokenInName
-            ),
-            Number(inAmount) // TODO: truncation ok?
-          ),
-          new Token(
-            chainId,
-            tokenOut,
-            tokenOutDecimals,
-            tokenOutSymbol,
-            tokenOutName
-          ),
-          TradeType.EXACT_INPUT,
-          swapOpts
-        );
+              inAmount,
+              tokenOut,
+            };
 
-        if (!route) {
-          throw new Error(
-            `No route found for swap. Token in: ${tokenIn}, Token out: ${tokenOut}. Amount in: ${inAmount}`
-          );
+            // If permit2 contract doesn't have high enough allowance, set to max for handler -> router
+            if (
+              (await erc20InContract.allowance(PERMIT2_ADDRESS)).toBigInt() <
+              inAmount
+            ) {
+              const approveAction: Action = {
+                contractAddress: tokenIn,
+                encodedFunction: erc20InContract.interface.encodeFunctionData(
+                  "approve",
+                  [PERMIT2_ADDRESS, ethers.constants.MaxUint256]
+                ),
+              };
+
+              resolve({
+                unwraps: [unwrap],
+                confidentialPayments: [],
+                actions: [approveAction, swapAction], // enqueue approve + swap
+                refundAssets: [refundAsset],
+                metadatas: [metadata],
+              });
+            } else {
+              resolve({
+                unwraps: [unwrap],
+                confidentialPayments: [],
+                actions: [swapAction],
+                refundAssets: [refundAsset],
+                metadatas: [metadata],
+              });
+            }
+          } catch (e) {
+            reject(e);
+          }
         }
-
-        const unwrap: UnwrapRequest = {
-          asset: AssetTrait.erc20AddressToAsset(tokenIn),
-          unwrapValue: inAmount,
-        };
-
-        const swapAction: Action = {
-          contractAddress: UNIVERSAL_SWAP_ROUTER_ADDRESS,
-          encodedFunction: route.methodParameters!.calldata,
-        };
-
-        const refundAsset = AssetTrait.erc20AddressToAsset(tokenOut);
-
-        const metadata: OperationMetadataItem = {
-          type: "Action",
-          actionType: "UniswapV3 Swap",
-          tokenIn,
-          inAmount,
-          tokenOut,
-        };
-
-        // If permit2 contract doesn't have high enough allowance, set to max for handler -> router
-        if (
-          (await erc20InContract.allowance(PERMIT2_ADDRESS)).toBigInt() <
-          inAmount
-        ) {
-          const approveAction: Action = {
-            contractAddress: tokenIn,
-            encodedFunction: erc20InContract.interface.encodeFunctionData(
-              "approve",
-              [PERMIT2_ADDRESS, ethers.constants.MaxUint256]
-            ),
-          };
-
-          resolve({
-            unwraps: [unwrap],
-            confidentialPayments: [],
-            actions: [approveAction, swapAction], // enqueue approve + swap
-            refundAssets: [refundAsset],
-            metadatas: [metadata],
-          });
-        } else {
-          resolve({
-            unwraps: [unwrap],
-            confidentialPayments: [],
-            actions: [swapAction],
-            refundAssets: [refundAsset],
-            metadatas: [metadata],
-          });
-        }
-      });
+      );
 
       this.pluginFn(prom);
 
