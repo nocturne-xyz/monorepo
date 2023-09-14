8f5a11c6df7145653ef45ee8c94e96f396c085e7
Luke Tchang <ltchang@stanford.edu>
2023-09-13 21:17:30 -0400

    test: add op request builder test for sequence of spends and refunds simulating subsequent swaps

--- packages/core/test/OpRequestBuilder.test.ts
+++ packages/core/test/OpRequestBuilder.test.ts
@@ -379,4 +379,51 @@ describe("OpRequestBuilder", () => {
 
     expect(opRequest.request).to.eql(expected);
   });
+
+  it("handles sequence of unwraps and refunds for same asset", async () => {
+    const sk = generateRandomSpendingKey();
+    const signer = new NocturneSigner(sk);
+    const refundAddr = signer.generateRandomStealthAddress();
+
+    // NOTE: because the below op builder request effectively swaps back and forth with the same 50
+    // tokens of the same asset, we only need to unwrap 50 tokens total and expect back the same 50
+    const expected: OperationRequest = {
+      joinSplitRequests: [
+        {
+          asset: shitcoin,
+          unwrapValue: 50n,
+        },
+        {
+          asset: ponzi,
+          unwrapValue: 50n,
+        },
+      ],
+      refunds: [
+        { encodedAsset: AssetTrait.encode(shitcoin), minRefundValue: 60n },
+        { encodedAsset: AssetTrait.encode(ponzi), minRefundValue: 50n },
+      ],
+      actions: [],
+      refundAddr: refundAddr,
+      chainId: 1n,
+      tellerContract: DUMMY_CONFIG.tellerAddress(),
+      deadline: 2n,
+    };
+
+    const provider = ethers.getDefaultProvider();
+    const builder = newOpRequestBuilder(provider, 1n, DUMMY_CONFIG);
+    const opRequest = await builder
+      .__unwrap(shitcoin, 50n) // -unwrap 50 shitcoin
+      .__refund({ asset: shitcoin, minRefundValue: 50n }) // +expect the 50 shitcoin back
+      .__unwrap(ponzi, 50n) // -unwrap 50 ponzi
+      .__refund({ asset: shitcoin, minRefundValue: 50n }) // +expect 50 more shitcoin (100 shitcoin)
+      .__unwrap(shitcoin, 50n) // -50 shitcoin from existing 100 (50 left)
+      .__refund({ asset: ponzi, minRefundValue: 100n }) // +get back 100 ponzi
+      .__unwrap(ponzi, 50n) // -use 50 of the 100 ponzi (50 left)
+      .__refund({ asset: shitcoin, minRefundValue: 10n }) // expect 10 more shitcoin back (60)
+      .refundAddr(refundAddr)
+      .deadline(2n)
+      .build();
+
+    expect(opRequest.request).to.eql(expected);
+  });
 });
