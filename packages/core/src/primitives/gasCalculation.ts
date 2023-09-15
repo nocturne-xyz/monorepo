69d7547414f338a04278429962b4faea9c0142d5
Luke Tchang <ltchang@stanford.edu>
2023-09-15 11:37:24 -0400

    adjust gas est for joinsplit + 10k since we added joinsplit info commit PI

--- packages/core/src/primitives/gasCalculation.ts
+++ packages/core/src/primitives/gasCalculation.ts
@@ -2,7 +2,7 @@ import { getTrackedAssets } from "./operation";
 import { Operation } from "./types";
 
 // Numbers and logic copied from contracts Types.sol
-const GAS_PER_JOINSPLIT_VERIFY_SINGLE = 290_000n;
+const GAS_PER_JOINSPLIT_VERIFY_SINGLE = 300_000n;
 const GAS_PER_JOINSPLIT_HANDLE = 110_000n;
 const GAS_PER_INSERTION_SUBTREE_UPDATE = 25_000n;
 const GAS_PER_INSERTION_ENQUEUE = 25_000n;
@@ -13,14 +13,14 @@ export function maxGasForOperation(op: Operation): bigint {
   const numJoinSplits = op.joinSplits.length;
   const numUniqueAssets = getTrackedAssets(op).length;
 
-  return BigInt(
+  return (
     op.executionGasLimit +
-      (GAS_PER_JOINSPLIT_VERIFY_SINGLE + GAS_PER_JOINSPLIT_HANDLE) *
-        BigInt(numJoinSplits) +
-      (GAS_PER_INSERTION_SUBTREE_UPDATE + GAS_PER_INSERTION_ENQUEUE) *
-        BigInt(numUniqueAssets + numJoinSplits * 2) +
-      GAS_PER_OPERATION_MISC +
-      GAS_BUFFER
+    (GAS_PER_JOINSPLIT_VERIFY_SINGLE + GAS_PER_JOINSPLIT_HANDLE) *
+      BigInt(numJoinSplits) +
+    (GAS_PER_INSERTION_SUBTREE_UPDATE + GAS_PER_INSERTION_ENQUEUE) *
+      BigInt(numUniqueAssets + numJoinSplits * 2) +
+    GAS_PER_OPERATION_MISC +
+    GAS_BUFFER
   );
 }
 
