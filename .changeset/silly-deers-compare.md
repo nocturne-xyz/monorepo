4d7147b625305d3828621acc918aab5ffb44f36b
Luke Tchang <ltchang@stanford.edu>
2023-09-20 22:28:07 -0400

    core adds correct gas accounting for extra gas joinsplits (#460)
    * op request gas prep checks how many joinsplits will be added and adds gas comp correspondingly
    
    * changeset
    
    * add test case, save
    
    * fix gas calculation/joinsplit update logic again
    
    * fix op request gas unit tests to have precise checks
    
    * fix: only attempt to gather more notes of existing asset if needed
    
    * fix spelling joinSplitsGas
    
    * replace throw errors with continue

---
+++ .changeset/silly-deers-compare.md
@@ -0,0 +1,5 @@
+---
+"@nocturne-xyz/core": patch
+---
+
+Op request gas prep checks how many joinsplits will be added for gas compensation and updates joinsplit requests accounting for new joinsplits
--- packages/core/src/primitives/gasCalculation.ts
+++ packages/core/src/primitives/gasCalculation.ts
@@ -7,14 +7,36 @@ const GAS_PER_JOINSPLIT_HANDLE = 110_000n;
 const GAS_PER_INSERTION_SUBTREE_UPDATE = 25_000n;
 const GAS_PER_INSERTION_ENQUEUE = 25_000n;
 const GAS_PER_OPERATION_MISC = 100_000n;
-const GAS_BUFFER = 100_000n;
+const GAS_BUFFER = 200_000n;
+
+interface GasCompensationParams {
+  executionGasLimit: bigint;
+  numJoinSplits: number;
+  numUniqueAssets: number;
+}
 
 export function maxGasForOperation(op: Operation): bigint {
   const numJoinSplits = op.joinSplits.length;
   const numUniqueAssets = getTrackedAssets(op).length;
 
+  return gasCompensationForParams({
+    executionGasLimit: op.executionGasLimit,
+    numJoinSplits,
+    numUniqueAssets,
+  });
+}
+
+export function gasCompensationForParams({
+  executionGasLimit,
+  numJoinSplits,
+  numUniqueAssets,
+}: GasCompensationParams): bigint {
+  console.log(
+    `Gas compensation equation: ${executionGasLimit} + (${GAS_PER_JOINSPLIT_VERIFY_SINGLE} + ${GAS_PER_JOINSPLIT_HANDLE}) * BigInt(${numJoinSplits}) + (${GAS_PER_INSERTION_SUBTREE_UPDATE} + ${GAS_PER_INSERTION_ENQUEUE}) * BigInt(${numUniqueAssets} + ${numJoinSplits} * 2) + ${GAS_PER_OPERATION_MISC} + ${GAS_BUFFER}`
+  );
+
   return (
-    op.executionGasLimit +
+    executionGasLimit +
     (GAS_PER_JOINSPLIT_VERIFY_SINGLE + GAS_PER_JOINSPLIT_HANDLE) *
       BigInt(numJoinSplits) +
     (GAS_PER_INSERTION_SUBTREE_UPDATE + GAS_PER_INSERTION_ENQUEUE) *
@@ -24,10 +46,7 @@ export function maxGasForOperation(op: Operation): bigint {
   );
 }
 
-export function maxGasForAdditionalJoinSplit(): bigint {
-  return (
-    GAS_PER_JOINSPLIT_VERIFY_SINGLE +
-    GAS_PER_JOINSPLIT_HANDLE +
-    3n * (GAS_PER_INSERTION_SUBTREE_UPDATE + GAS_PER_INSERTION_ENQUEUE) // 2 new NCs + refund
-  );
-}
+export const MAX_GAS_FOR_ADDITIONAL_JOINSPLIT =
+  GAS_PER_JOINSPLIT_VERIFY_SINGLE +
+  GAS_PER_JOINSPLIT_HANDLE +
+  3n * (GAS_PER_INSERTION_SUBTREE_UPDATE + GAS_PER_INSERTION_ENQUEUE); // 2 new NCs + refund
