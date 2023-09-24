4fb9ed37d6c86fc952853e03145e2f278a20f098
danielpark <danielpark1140@gmail.com>
2023-09-24 09:44:55 -0700

    snapshot work

--- actors/deposit-screener/test/RULESET_V1.test.ts
+++ actors/deposit-screener/test/RULESET_V1.test.ts
@@ -1,51 +1,22 @@
 import { expect } from "chai";
-import { ScreeningDepositRequest } from "../src";
+import fs from "fs";
+import path from "path";
 import { CachedApiCallData } from "../src/screening/checks/RuleSet";
+import { API_CALL_MAP, ApiCallNames } from "../src/screening/checks/apiCalls";
 import { RULESET_V1 } from "../src/screening/checks/v1/RULESET_V1";
 import apiCallsSnapshot from "./snapshots/apiCallsSnapshot09-13-2023.json";
-
-// Address contexts provided in: notion.so/nocturnelabs/Compliance-Provider-Evaluation-9ffe8bbf698f420498eba9e782e93b6d
-
-export const formDepositInfo = (
-  spender: string,
-  value = 0n
-): ScreeningDepositRequest => {
-  return {
-    spender,
-    assetAddr: "",
-    value,
-  } as const;
-};
-
-export const REJECT_ADDRESSES = {
-  ROCKETSWAP: "0x96c0876f573e27636612cf306c9db072d2b13de8",
-  ZUNAMI: "0x96c0876f573e27636612cf306c9db072d2b13de8",
-  ZUNAMI_2ND_DEGREE: "0xF00d0e11AcCe1eA37658f428d947C3FFFAeaDe70",
-  STEADEFI: "0xE10d4a5bd440775226C7e1858f573E379d0aca36",
-  EARNING_FARM: "0xee4b3dd20902Fa3539706F25005fa51D3b7bDF1b",
-  SUS_TC_USER: "0x5f1237bb7c14d4b4ae0026a186abc9c27a4b1224",
-  SWIRLEND: "0x26f6d954c4132fae4efe389b947c8cc4b4ce5ce7",
-  AZTEC_4: "0x8C9555D210C9019f952b0cCF57f8E65D542281F2",
-  TC_1: "0x86738d21db9a2ccc9747b2e374fd1a500f6eeb50",
-  TC_4: "0xa9b4b8108b6df063525aea9bac68b0e03b65e0c5",
-  TC_6: "0x698739c0F2e92446f6696578c89308A05F5BA0Fd",
-  TC_7: "0xadd7885af8f37df5c965e5d16caf16f807dc79a0",
-} as const;
-
-export const APPROVE_ADDRESSES = {
-  VITALIK: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-  BEIKO: "0x10F5d45854e038071485AC9e402308cF80D2d2fE",
-  TC_2: "0xEE6572fD080F791E10B48F789a9C2eF76114bA86",
-  TC_3: "0x3f77d1F729B439dA80264622dEACe480153e683D",
-  TC_5: "0x5E1B70EA7F694951ebAC269BEb2b3F4f25dD6e6a",
-  AZTEC_2: "0xd81A68F256985452E82297b43A465DE4F2a6Fd24",
-  AZTEC_1: "0x7c3171A6eabc8fc95077762ACF4B04eE1eAEF465",
-  AZTEC_3: "0xa0bE23dB857262c8ff29763930fCD04Cc621FcCA",
-} as const;
-
-type ScreeningTestCaseAddresses =
-  | (typeof REJECT_ADDRESSES)[keyof typeof REJECT_ADDRESSES]
-  | (typeof APPROVE_ADDRESSES)[keyof typeof APPROVE_ADDRESSES];
+import {
+  APPROVE_ADDRESSES,
+  AddressDataSnapshot,
+  CachedAddressData,
+  REJECT_ADDRESSES,
+  ScreeningTestCaseAddresses,
+  TEST_ADDRESSES,
+  formDepositInfo,
+  getLatestSnapshotFolder,
+  saveSnapshot,
+} from "./utils";
+import { sleep } from "@nocturne-xyz/core";
 
 const MOCK_API_CALLS = apiCallsSnapshot as Record<
   ScreeningTestCaseAddresses,
@@ -53,6 +24,54 @@ const MOCK_API_CALLS = apiCallsSnapshot as Record<
 >;
 
 describe("RULESET_V1", () => {
+  let snapshotData: AddressDataSnapshot = {};
+
+  before(async () => {
+    if (process.env.SNAPSHOT_ADDRESSES === "true") {
+      const numAddresses = Object.keys(TEST_ADDRESSES).length;
+      console.log(
+        "Test suite invoked with SNAPSHOT_ADDRESSES=true, running API calls and saving snapshot..."
+      );
+      console.log(`There are ${numAddresses} addresses to snapshot`);
+      let count = 0;
+      for (const address of Object.values(TEST_ADDRESSES)) {
+        console.log(
+          `Starting API calls for address: ${address}—${count} of ${numAddresses}`
+        );
+        const deposit = formDepositInfo(address);
+        snapshotData[address] = {};
+        for (const [callName, apiCall] of Object.entries(API_CALL_MAP)) {
+          if (
+            callName === API_CALL_MAP.MISTTRACK_ADDRESS_OVERVIEW.name ||
+            callName === API_CALL_MAP.MISTTRACK_ADDRESS_RISK_SCORE.name
+          ) {
+            console.log(
+              "Sleeping for 5 seconds to avoid Misttrack rate limit..."
+            );
+            await sleep(5000);
+          }
+          const addressData = snapshotData[address] as CachedAddressData;
+          console.log(`Calling ${callName} for ${address}...`);
+          addressData[callName as ApiCallNames] = await apiCall(deposit);
+
+          console.log(`Successfully called ${callName} for ${address}`);
+        }
+      }
+      console.log("All API calls completed, saving snapshot...");
+      saveSnapshot(snapshotData);
+      console.log("Snapshot saved successfully");
+    } else {
+      const folderPath = (await getLatestSnapshotFolder("./snapshots")) ?? "";
+      const filePath = path.join(folderPath, "snapshot.json");
+      if (fs.existsSync(filePath)) {
+        const rawData = fs.readFileSync(filePath, "utf-8");
+        snapshotData = JSON.parse(rawData);
+      } else {
+        throw new Error("No snapshot files found");
+      }
+    }
+  });
+
   describe("Rejections", () => {
     it("should reject RocketSwap exploiter", async () => {
       const result = await RULESET_V1.check(
