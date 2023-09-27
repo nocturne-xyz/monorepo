c24c8c787880d7182c0e2ec001d9a1f79fd5cbb6
Luke Tchang <ltchang@stanford.edu>
2023-09-27 09:31:28 -0400

    move poseidon ext out of test dir

--- packages/contracts/contracts/test/utils/PoseidonHashers.sol
+++ packages/contracts/contracts/test/utils/PoseidonHashers.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "../interfaces/IHasher.sol";
-import "../interfaces/IHasherExt.sol";
 import "../interfaces/IPoseidon.sol";
-import "../interfaces/IPoseidonExt.sol";
 
 contract PoseidonHasherT3 is IHasherT3 {
     IPoseidonT3 public poseidonT3;
@@ -73,50 +71,5 @@ contract PoseidonHasherT7 is IHasherT7 {
         uint256[6] memory elems
     ) external view override returns (uint256) {
         return poseidonT7.poseidon(elems);
-    }
-}
-
-contract PoseidonExtHasherT3 is IHasherExtT3 {
-    IPoseidonExtT3 public poseidonExtT3;
-
-    constructor(address _poseidonExtT3) {
-        poseidonExtT3 = IPoseidonExtT3(_poseidonExtT3);
-    }
-
-    function hash(
-        uint256 initialState,
-        uint256[2] memory elems
-    ) external view override returns (uint256) {
-        return poseidonExtT3.poseidonExt(initialState, elems);
-    }
-}
-
-contract PoseidonExtHasherT4 is IHasherExtT4 {
-    IPoseidonExtT4 public poseidonExtT4;
-
-    constructor(address _poseidonExtT4) {
-        poseidonExtT4 = IPoseidonExtT4(_poseidonExtT4);
-    }
-
-    function hash(
-        uint256 initialState,
-        uint256[3] memory elems
-    ) external view override returns (uint256) {
-        return poseidonExtT4.poseidonExt(initialState, elems);
-    }
-}
-
-contract PoseidonExtHasherT7 is IHasherExtT7 {
-    IPoseidonExtT7 public poseidonExtT7;
-
-    constructor(address _poseidonExtT7) {
-        poseidonExtT7 = IPoseidonExtT7(_poseidonExtT7);
-    }
-
-    function hash(
-        uint256 initialState,
-        uint256[6] memory elems
-    ) external view override returns (uint256) {
-        return poseidonExtT7.poseidonExt(initialState, elems);
     }
 }
