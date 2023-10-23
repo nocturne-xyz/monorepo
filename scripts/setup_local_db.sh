6b4b8a9d66cb2db0459bd2749066e7ca04abe539
danielpark <danielpark1140@gmail.com>
2023-10-23 15:14:10 -0700

    tidy up script

--- scripts/setup_local_db.sh
+++ scripts/setup_local_db.sh
@@ -9,23 +9,21 @@ fi
 
 FLYWAY_PATH=$1
 
-# Step 1: Install PostgreSQL
+# Step 1: install & start psql
 brew install postgresql
-
-# Step 2: Start PostgreSQL
 brew services start postgresql
 
-# Step 3: Create symlink for Flyway CLI
+# Step 3: create symlink for Flyway CLI
 ln -s $FLYWAY_PATH /usr/local/bin/flyway
 
-# Step 5: Create db and user
+# Step 5: create db and user
 createdb nocturne
 psql -d nocturne -c "CREATE USER postgres WITH ENCRYPTED PASSWORD 'postgres';"
 psql -d nocturne -c "GRANT ALL PRIVILEGES ON DATABASE nocturne TO postgres;"
 psql -d nocturne -c "ALTER USER postgres CREATEROLE CREATEDB;"
 
-# Step 6: Run migrations
+# Step 6: run migrations
 flyway migrate -placeholders.nocturne_db_user_password=password
 
-# Done
-echo "Dev environment setup complete."
+# done
+echo "Dev environment setup complete!✅"
