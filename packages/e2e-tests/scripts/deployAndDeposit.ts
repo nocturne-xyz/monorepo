cd1a9876ed6dc82386941b579c14b42074770332
Luke Tchang <ltchang@stanford.edu>
2023-10-25 16:44:56 -0400

    Revert "fix e2e tests"
    This reverts commit 756a070454bb641175fce9db6bcfbaeddcd6d669.

--- packages/e2e-tests/scripts/deployAndDeposit.ts
+++ packages/e2e-tests/scripts/deployAndDeposit.ts
@@ -69,6 +69,7 @@ const TEST_CANONICAL_NOCTURNE_ADDRS: CanonAddress[] = [
   const [config, { erc20, gasToken }, { depositManager }] =
     await deployContractsWithDummyConfig(deployerEoa, {
       screeners: [DEPOSIT_SCREENER],
+      bundlers: [BUNDLER],
       subtreeBatchFillers: [deployerEoa.address, SUBTREE_BATCH_FILLER],
     });
 
