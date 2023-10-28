adedeb270b0b3742bd4627fcca168f6784f2acc9
Luke Tchang <ltchang@stanford.edu>
2023-10-28 15:24:20 -0400

    update goerli config

--- packages/deploy/scripts/verify.sh
+++ packages/deploy/scripts/verify.sh
@@ -4,7 +4,7 @@
 # and artifacts for the contracts. ETHERSCAN_API_KEY must be set in the contracts package .env file 
 # for this script to work.
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
-CONTRACTS_DIR="$SCRIPT_DIR/../../contracts"
+CONTRACTS_DIR="$SCRIPT_DIR/../../../../protocol/packages/contracts"
 cd "$CONTRACTS_DIR"
 
 cd "$CONTRACTS_DIR"
