d4c5fba5d0ed77329be93195245bab83a630496c
Sladuca <sladuca777@gmail.com>
2023-11-01 17:49:56 -0400

    always use rapidsnark in `build_docker_updater.sh`

--- scripts/build_docker_updater.sh
+++ scripts/build_docker_updater.sh
@@ -7,50 +7,30 @@ cd $ROOT_DIR
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-usage() { echo "usage: $0 [-p <'mock' | 'rapidsnark'>" 1>&2; }
+echo "building subtree updater with rapidsnark"
 
-while getopts ":p:o:" o; do
-    case "${o}" in
-        p)
-            SUBTREE_UPDATER_PROVER_MODE=${OPTARG}
-            ((SUBTREE_UPDATE_PROVER_MODE == "mock" || SUBTREE_UPDATE_PROVER_MODE == "rapidsnark")) || (usage && exit 1)
-            ;;
-        o)
-            BUILD_ONLY=${OPTARG}
-            ((BUILD_ONLY == "subtree-updater" || BUILD_ONLY == "bundler" || BUILD_ONLY == "deposit-screener" || BUILD_ONLY == "test-actor")) || (usage && exit 1)
-            ;;
-        *)
-            SUBTREE_UPDATER_PROVER_MODE="mock"
-            ;;
-    esac
-done
+pushd rapidsnark
+git submodule init
+git submodule update
+popd
 
-if [ "$SUBTREE_UPDATER_PROVER_MODE" == "rapidsnark" ]; then
-    pushd rapidsnark
-    git submodule init
-    git submodule update
+if [ ! -d "$ROOT_DIR/circuit-artifacts/subtreeupdate/" ]; then
+    pushd packages/circuits
+    yarn download-big-ptau
+    yarn build:subtreeupdate
+    yarn local-prover:gen-test-cases
     popd
+fi
 
-    if [ ! -d "$ROOT_DIR/circuit-artifacts/subtreeupdate/" ]; then
-        pushd packages/circuits
-        yarn download-big-ptau
-        yarn build:subtreeupdate
-        yarn local-prover:gen-test-cases
-        popd
-    fi
+if [[ $(uname -m) == 'arm64' ]]; then
+    echo "dected arm64, building for x86..."
 
-    if [[ $(uname -m) == 'arm64' ]]; then
-        echo "dected arm64, building for x86..."
-
-        docker build --platform linux/amd64 -t rapidsnark ./rapidsnark
-        docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile --platform linux/amd64 -t subtree-updater:$COMMIT_HASH .
-    else
-        echo "deceting x86, building for x86..."
-        docker build -t rapidsnark ./rapidsnark
-        docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile -t subtree-updater:$COMMIT_HASH .
-    fi
-else 
-    docker build -f actors/subtree-updater/Mock.Dockerfile -t subtree-updater:$COMMIT_HASH .
+    docker build --platform linux/amd64 -t rapidsnark ./rapidsnark
+    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile --platform linux/amd64 -t subtree-updater:$COMMIT_HASH .
+else
+    echo "deceting x86, building for x86..."
+    docker build -t rapidsnark ./rapidsnark
+    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile -t subtree-updater:$COMMIT_HASH .
 fi
 
 docker tag subtree-updater:$COMMIT_HASH 714567495486.dkr.ecr.us-east-2.amazonaws.com/subtree-updater:$COMMIT_HASH
