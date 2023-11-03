a4e1e3105be803515567034b84f3b1b95d3ef1ab
Sladuca <sladuca777@gmail.com>
2023-11-03 10:46:12 -0400

    implement new method in rpc sync adapter

--- sync/rpc-sync-adapters/src/adapter.ts
+++ sync/rpc-sync-adapters/src/adapter.ts
@@ -191,6 +191,19 @@ export class RPCSDKSyncAdapter implements SDKSyncAdapter {
     const currentBlock = await this.handlerContract.provider.getBlockNumber();
     return currentBlock;
   }
+
+  async getLatestIndexedMerkleIndex(finalityBlocks?: number): Promise<number> {
+    // get the latest block number minus `finalityBlocks`, and fetch the current merkle count from the contract at that block
+    const currentBlock =
+      (await this.handlerContract.provider.getBlockNumber()) -
+      (finalityBlocks ?? 0);
+
+    // TODO: figure out a way to do this without relying on `blockTag` - not all nodes support this and the ones that do are much more expensive
+    const count = await this.handlerContract.totalCount({
+      blockTag: currentBlock,
+    });
+    return count.toNumber() - 1;
+  }
 }
 
 function extractNullifiersFromJoinSplitEvents(
