cc1aa7aeb77cfd3addd47dae1bf44a70d7491eba
danielpark <danielpark1140@gmail.com>
2023-11-07 15:59:11 -0800

    Update eoa sig keygen message

--- packages/frontend-sdk/src/eoaSigKeygen.ts
+++ packages/frontend-sdk/src/eoaSigKeygen.ts
@@ -1,13 +1,19 @@
 import { ethers } from "ethers";
 
 const SPEND_KEY_FIXED_MESSAGE = Buffer.from(
-  "Sign this message to generate your Nocturne Spending Key. This key lets the application spend your funds in Nocturne.\n\nIMPORTANT: ONLY SIGN THIS MESSAGE IF YOU TRUST THE APPLICATION."
+  `Generate your Spending Key
+
+  1. Verify you're on: https://app.nocturne.xyz before proceeding
+  2. Verify that you have access to the connected wallet's private key. This spending key is unique to your connected wallet address. 
+  3. Nocturne cannot recover your spending key or your wallet's private key for you.
+
+  By signing, you will generate your spending key to manage Nocturne funds.`,
 );
 
 export async function generateNocturneSpendKeyFromEoaSig(
-  signer: ethers.Signer
+  signer: ethers.Signer,
 ): Promise<string> {
   return ethers.utils.keccak256(
-    await signer.signMessage(SPEND_KEY_FIXED_MESSAGE)
+    await signer.signMessage(SPEND_KEY_FIXED_MESSAGE),
   );
 }
