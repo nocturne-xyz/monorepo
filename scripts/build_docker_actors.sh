73d364c26803b1a4c12ecc8f5da85f4140b36783
Sladuca <sladuca777@gmail.com>
2023-11-07 15:20:18 -0500

    build containers specific to each network

--- scripts/build_docker_actors.sh
+++ scripts/build_docker_actors.sh
@@ -2,10 +2,19 @@
 
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ROOT_DIR="$SCRIPT_DIR/../"
+NETWORK_NAME=$1
+
+if [ -z "$NETWORK_NAME" ]
+then
+	echo "no network name given"
+	exit 1
+else
+	echo "building actor container for network '$NETWORK_NAME'"
+fi
 
 cd $ROOT_DIR
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-docker build -t nocturne-actors:$COMMIT_HASH .
-docker tag nocturne-actors:$COMMIT_HASH 714567495486.dkr.ecr.us-east-2.amazonaws.com/nocturne-actors:$COMMIT_HASH
+docker build -t nocturne-actors:$NETWORK_NAME-$COMMIT_HASH .
+docker tag nocturne-actors:$NETWORK_NAME-$COMMIT_HASH 714567495486.dkr.ecr.us-east-2.amazonaws.com/nocturne-actors:$NETWORK_NAME-$COMMIT_HASH
--- scripts/build_docker_updater.sh
+++ scripts/build_docker_updater.sh
@@ -2,6 +2,15 @@
 
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ROOT_DIR="$SCRIPT_DIR/../"
+NETWORK_NAME=$1
+
+if [ -z "$NETWORK_NAME" ]
+then
+	echo "no network name given"
+	exit 1
+else
+	echo "building updater container for network '$NETWORK_NAME'"
+fi
 
 cd $ROOT_DIR
 
@@ -26,11 +35,11 @@ if [[ $(uname -m) == 'arm64' ]]; then
     echo "dected arm64, building for x86..."
 
     docker build --platform linux/amd64 -t rapidsnark ./rapidsnark
-    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile --platform linux/amd64 -t subtree-updater:$COMMIT_HASH .
+    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile --platform linux/amd64 -t subtree-updater:$NETWORK_NAME-$COMMIT_HASH .
 else
     echo "deceting x86, building for x86..."
     docker build -t rapidsnark ./rapidsnark
-    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile -t subtree-updater:$COMMIT_HASH .
+    docker buildx build --build-context circuit-artifacts=circuit-artifacts -f actors/subtree-updater/Dockerfile -t subtree-updater:$NETWORK_NAME-$COMMIT_HASH .
 fi
 
-docker tag subtree-updater:$COMMIT_HASH 714567495486.dkr.ecr.us-east-2.amazonaws.com/subtree-updater:$COMMIT_HASH
+docker tag subtree-updater:$NETWORK_NAME-$COMMIT_HASH 714567495486.dkr.ecr.us-east-2.amazonaws.com/subtree-updater:$NETWORK_NAME-$COMMIT_HASH
