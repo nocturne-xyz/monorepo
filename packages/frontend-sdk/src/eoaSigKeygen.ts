781e93e2d10624c6a220c5cc5c0a4675bb97b838
danielpark <danielpark1140@gmail.com>
2023-11-07 17:52:38 -0800

    update text

--- packages/frontend-sdk/src/eoaSigKeygen.ts
+++ packages/frontend-sdk/src/eoaSigKeygen.ts
@@ -7,7 +7,7 @@ const SPEND_KEY_FIXED_MESSAGE =
 By signing this message, I assert that
 1. I trust the application
 2. I have safely stored the private key (or seed phrase from which the private key was derived) for the connected Ethereum account
-3. The only way I can recover access to my Nocturne account is by signing this message again with the current Ethereum account` as const;
+3. The only way I can recover access to this Nocturne account is by signing this message again with the same Ethereum account mentioned in #2.` as const;
 
 export async function generateNocturneSpendKeyFromEoaSig(
   signer: ethers.Signer,
