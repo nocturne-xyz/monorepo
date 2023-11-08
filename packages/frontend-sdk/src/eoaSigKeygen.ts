571ebc33c63142654edf244bf896e886635ed979
danielpark <danielpark1140@gmail.com>
2023-11-07 17:44:54 -0800

    warn

--- packages/frontend-sdk/src/eoaSigKeygen.ts
+++ packages/frontend-sdk/src/eoaSigKeygen.ts
@@ -1,18 +1,18 @@
 import { ethers } from "ethers";
 
-const SPEND_KEY_FIXED_MESSAGE = Buffer.from(
+// ! ⚠️⚠️⚠️ THIS MESSAGE CANNOT BE CHANGED! KEY DERIVATION IS DICTATED BY THE EXACT TEXT ⚠️⚠️⚠️
+const SPEND_KEY_FIXED_MESSAGE =
   `Sign to generate your Nocturne spending key. This key will secure your funds in Nocturne.
 
 By signing this message, I assert that
 1. I trust the application
 2. I have safely stored the private key (or seed phrase from which the private key was derived) for the connected Ethereum account
-3. The only way I can recover access to my Nocturne account is by signing this message again with the current Ethereum account`,
-);
+3. The only way I can recover access to my Nocturne account is by signing this message again with the current Ethereum account` as const;
 
 export async function generateNocturneSpendKeyFromEoaSig(
   signer: ethers.Signer,
 ): Promise<string> {
   return ethers.utils.keccak256(
-    await signer.signMessage(SPEND_KEY_FIXED_MESSAGE),
+    await signer.signMessage(Buffer.from(SPEND_KEY_FIXED_MESSAGE)),
   );
 }
