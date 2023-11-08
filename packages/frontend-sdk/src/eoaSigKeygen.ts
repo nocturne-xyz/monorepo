b69ac2e88a798a8eb6677738418c1b44146ec603
Daniel Park <danielpark1140@gmail.com>
2023-11-08 11:52:56 -0800

    Update eoa sig keygen message (#580)
    * Update eoa sig keygen message
    
    * changeset
    
    * Update eoaSigKeygen.ts
    
    Signed-off-by: Daniel Park <danielpark1140@gmail.com>
    
    * warn
    
    * update text
    
    * this -> your
    
    * Update eoaSigKeygen.ts
    
    Signed-off-by: Daniel Park <danielpark1140@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Park <danielpark1140@gmail.com>

--- packages/frontend-sdk/src/eoaSigKeygen.ts
+++ packages/frontend-sdk/src/eoaSigKeygen.ts
@@ -1,13 +1,18 @@
 import { ethers } from "ethers";
 
-const SPEND_KEY_FIXED_MESSAGE = Buffer.from(
-  "Sign this message to generate your Nocturne Spending Key. This key lets the application spend your funds in Nocturne.\n\nIMPORTANT: ONLY SIGN THIS MESSAGE IF YOU TRUST THE APPLICATION."
-);
+// ! ⚠️⚠️⚠️ THIS MESSAGE CANNOT BE CHANGED! KEY DERIVATION IS DICTATED BY THE EXACT TEXT ⚠️⚠️⚠️
+const SPEND_KEY_FIXED_MESSAGE =
+  `Sign to generate your Nocturne spending key. This key will secure your funds in Nocturne.
+
+By signing this message, I assert that
+1. I trust the application
+2. I have safely stored the private key (or seed phrase from which the private key was derived) for the connected Ethereum account
+3. The only way I can recover access to my Nocturne account is by signing this message again with the same Ethereum account mentioned in #2.` as const;
 
 export async function generateNocturneSpendKeyFromEoaSig(
-  signer: ethers.Signer
+  signer: ethers.Signer,
 ): Promise<string> {
   return ethers.utils.keccak256(
-    await signer.signMessage(SPEND_KEY_FIXED_MESSAGE)
+    await signer.signMessage(Buffer.from(SPEND_KEY_FIXED_MESSAGE)),
   );
 }
