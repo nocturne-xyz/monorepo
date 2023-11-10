e69bcb2f6e175a2babf9332ac98a1da93d11e4db
Sebastien La Duca <sladuca777@gmail.com>
2023-11-10 11:14:37 -0500

    fix copy-circuit-artifacts script (#585)

---
+++ packages/frontend-sdk/scripts/copy_circuit_artifacts.sh
@@ -0,0 +1,7 @@
+cp ../../circuit-artifacts/joinsplit/joinsplit_cpp/vkey.json ./circuit-artifacts/joinsplit/joinsplitVkey.json
+cp ../../circuit-artifacts/joinsplit/joinsplit_js/joinsplit.wasm ./circuit-artifacts/joinsplit/joinsplit.wasm
+cp ../../circuit-artifacts/joinsplit/joinsplit_cpp/joinsplit.zkey ./circuit-artifacts/joinsplit/joinsplit.zkey
+
+cp ../../circuit-artifacts/canonAddrSigCheck/canonAddrSigCheck_cpp/vkey.json ./circuit-artifacts/canonAddrSigCheck/canonAddrSigCheckVkey.json
+cp ../../circuit-artifacts/canonAddrSigCheck/canonAddrSigCheck_js/canonAddrSigCheck.wasm ./circuit-artifacts/canonAddrSigCheck/canonAddrSigCheck.wasm
+cp ../../circuit-artifacts/canonAddrSigCheck/canonAddrSigCheck_cpp/canonAddrSigCheck.zkey ./circuit-artifacts/canonAddrSigCheck/canonAddrSigCheck.zkey
