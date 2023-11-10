f4ce0024f76cd76e0b8ffc4600644fb20c184a53
Luke Tchang <ltchang@stanford.edu>
2023-11-10 16:37:06 -0500

    rename

--- actors/bundler/src/sanctions.ts
+++ actors/bundler/src/sanctions.ts
@@ -2,7 +2,7 @@ import { Address } from "@nocturne-xyz/core";
 import * as ethers from "ethers";
 import SanctionsListAbi from "./abis/SanctionsList.json";
 
-const SANCTIONS_LIST_CONTRACT_ADDRS: Record<number, Address> = {
+const CHAIN_ID_TO_SANCTIONS_LIST_CONTRACT: Record<number, Address> = {
   1: "0x40c57923924b5c5c5455c48d93317139addac8fb",
 };
 
@@ -12,12 +12,12 @@ export async function isSanctionedAddress(
 ): Promise<boolean> {
   const chainId = (await provider.getNetwork()).chainId;
   // skip check if there's no sanctions contract on this chain
-  if (!SANCTIONS_LIST_CONTRACT_ADDRS[chainId]) {
+  if (!CHAIN_ID_TO_SANCTIONS_LIST_CONTRACT[chainId]) {
     return false;
   }
 
   const contract = new ethers.Contract(
-    SANCTIONS_LIST_CONTRACT_ADDRS[chainId],
+    CHAIN_ID_TO_SANCTIONS_LIST_CONTRACT[chainId],
     SanctionsListAbi,
     provider
   );
