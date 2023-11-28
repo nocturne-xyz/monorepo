db9fc260b035c1771d441d51e52501e362c30888
Sladuca <sladuca777@gmail.com>
2023-11-28 16:22:57 -0500

    add script that does all the things

---
+++ scripts/build_and_push_all.sh
@@ -0,0 +1,20 @@
+
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOT_DIR="$SCRIPT_DIR/../"
+NETWORK_NAME=$1
+
+if [ -z "$NETWORK_NAME" ]
+then
+	echo "no network name given"
+	exit 1
+else
+	echo "building and pushing all images for network '$NETWORK_NAME'"
+fi
+
+yarn checkout-circuit-artifacts:$NETWORK_NAME
+./scripts/authenticate_ecr.sh
+
+yarn build:docker-actors:$NETWORK_NAME
+yarn push:docker-actors:$NETWORK_NAME
+yarn build:docker-updater:$NETWORK_NAME
+yarn push:docker-updater:$NETWORK_NAME
