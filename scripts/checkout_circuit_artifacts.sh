1c02caa074dd928d7ab9d7afad34404fc378c8fc
Sladuca <sladuca777@gmail.com>
2023-11-29 12:54:45 -0500

    cache circuit artifacts when checking out

--- scripts/checkout_circuit_artifacts.sh
+++ scripts/checkout_circuit_artifacts.sh
@@ -1,12 +1,32 @@
-SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-NETWORK_NAME=$1
+#!/bin/bash
 
-if [ -z "$NETWORK_NAME" ]
-then
-	echo "no network name given"
-	exit 1
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOT_DIR="$SCRIPT_DIR/../"
+NETWORK_NAME="$1"
+
+if [ -z "$NETWORK_NAME" ]; then
+    echo "No network name given"
+    exit 1
 else
-	echo "checking out circuit artifacts for network '$NETWORK_NAME'"
+    echo "Checking out circuit artifacts for network '$NETWORK_NAME'"
 fi
 
-aws s3 cp s3://actor-circuit-artifacts-$NETWORK_NAME/ ./circuit-artifacts --recursive
+CACHE_DIR="$ROOT_DIR/cache"
+ARTIFACTS_DIR="$ROOT_DIR/circuit-artifacts"
+
+# Check if the cache directory exists, create it if not
+if [ ! -d "$CACHE_DIR" ]; then
+    mkdir -p "$CACHE_DIR"
+fi
+
+CACHE_ENTRY="$CACHE_DIR/circuit-artifacts-$NETWORK_NAME"
+
+# Check if the cache entry exists. if it doesn't, download artifacts into it
+if [ ! -d "$CACHE_ENTRY" ]; then
+    # Download artifacts from S3
+    aws s3 cp "s3://actor-circuit-artifacts-$NETWORK_NAME/" "$CACHE_ENTRY" --recursive
+fi
+
+# Copy cached artifacts to the working directory
+cp -r "$CACHE_ENTRY"/** "$ARTIFACTS_DIR"
+
