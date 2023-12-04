17f2bd954d84777cd433cf321fa6472b56b0a266
Luke Tchang <ltchang@stanford.edu>
2023-12-04 15:05:52 -0500

    fix again

--- sql/migrate-with-retry.sh
+++ sql/migrate-with-retry.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# note - this is necessary for local dev and should not really be necessary for deployed environments
-#  this is here because even though in docker compose you can provide `depends_on` the postgres container
-#  will start up and be look ready but the postgres process will not be ready to accept connections
-
 max_attempts=3
 delay=1
 attempt=1
@@ -15,15 +11,17 @@ do
    output=$(flyway migrate -placeholders.nocturne_db_user_password="$NOCTURNE_DB_USER_PASSWORD" 2>&1)
    result=$?
    
-   echo "$output"  # Print the output whether it's a success or an error
-   echo "$result"
+   # Print the output whether it's a success or an error
+   echo "$output" 
 
    if [ $result -eq 0 ]
    then
      echo "Migration successful"
      exit 0
    else
-     echo "Migration failed with status $result, check the output above."
+     echo "Migration failed with status $result"
+     # Print error message if the command fails
+     echo "Error: $output"
    fi
 
    echo "Migration failed, retrying in $delay seconds..."
