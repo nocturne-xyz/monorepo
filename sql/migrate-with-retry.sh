e616b2742e49a95d62c1d480b2965137010d6a9e
Luke Tchang <ltchang@stanford.edu>
2023-12-04 17:50:39 -0500

    fix again

--- sql/migrate-with-retry.sh
+++ sql/migrate-with-retry.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
 
 max_attempts=3
-delay=5 # Adjust the delay as necessary
+delay=5 # Delay time in seconds
 attempt=1
+output_log="migrate_output.log"
 
 while [ $attempt -le $max_attempts ]; do
    echo "Attempt $attempt/$max_attempts"
    
-   # Run the Flyway command with verbose logging (-X)
-   flyway migrate -X -placeholders.nocturne_db_user_password="$NOCTURNE_DB_USER_PASSWORD" > migrate_output.log 2>&1
+   # Run the Flyway command with verbose logging (-X), and redirect all output to a log file
+   flyway migrate -X -placeholders.nocturne_db_user_password="$NOCTURNE_DB_USER_PASSWORD" > "$output_log" 2>&1
    result=$?
    
-   # Wait a bit to ensure all output is flushed
+   # Sleep for a while to ensure all output is written to the log file
    sleep $delay
    
-   # Read the full output from the log file
-   output=$(<migrate_output.log)
-   
-   # Print the output
-   echo "Output: $output"
+   # Check if the output log file exists and is not empty
+   if [ -s "$output_log" ]; then
+     # Read the full output from the log file
+     output=$(cat "$output_log")
+     
+     # Print the output
+     echo "Output:"
+     echo "$output"
+   else
+     echo "No output was captured in the log file."
+   fi
 
+   # Check the result and decide if we should continue
    if [ $result -eq 0 ]; then
      echo "Migration successful"
      exit 0
@@ -27,10 +35,12 @@ while [ $attempt -le $max_attempts ]; do
      echo "Migration failed with status $result"
    fi
 
+   # If the migration failed, wait before retrying
    echo "Migration failed, retrying in $delay seconds..."
    sleep $delay
    attempt=$(( attempt + 1 ))
 done
 
+# If all attempts fail, print a message and exit with the last error code
 echo "Migration failed after $max_attempts attempts"
 exit $result
