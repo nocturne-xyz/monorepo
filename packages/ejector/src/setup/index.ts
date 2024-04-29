11434ab88b133ed37056002443bb64c98c07fc54
Sladuca <sladuca777@gmail.com>
2024-04-29 10:12:48 -0400

    add missing argument

--- packages/ejector/src/setup/index.ts
+++ packages/ejector/src/setup/index.ts
@@ -3,6 +3,8 @@ import { downloadCircuitArtifacts } from "./downloadCircuitArtifacts";
 export type SetupArgs = {
   skipSubtreeUpdateCircuit: boolean;
 };
-export async function setup(args: SetupArgs): Promise<void> {
+export async function setup(
+  args: SetupArgs = { skipSubtreeUpdateCircuit: true }
+): Promise<void> {
   await Promise.all([downloadCircuitArtifacts(args.skipSubtreeUpdateCircuit)]);
 }
